ddbb31349bfa388d4b27fc5e33d805c0374fc1ce
Sam Foreman <saforem2@gmail.com>
2019-04-30 00:09:26 -0500

    Fixed bug where `log_dir` was being created twice inside `loggers/train_logger.py`.

--- l2hmc-qcd/loggers/train_logger.py
+++ l2hmc-qcd/loggers/train_logger.py
@@ -73,15 +73,15 @@ class TrainLogger:
 
     def _create_dir_structure(self, log_dir):
         """Create relevant directories for storing data."""
-        project_dir = os.path.abspath(os.path.dirname(FILE_PATH))
-        root_log_dir = os.path.join(project_dir, log_dir)
-        io.check_else_make_dir(root_log_dir)
+        #  project_dir = os.path.abspath(os.path.dirname(FILE_PATH))
+        #  root_log_dir = os.path.join(project_dir, log_dir)
+        #  io.check_else_make_dir(root_log_dir)
 
-        run_num = io.get_run_num(root_log_dir)
-        log_dir = os.path.abspath(os.path.join(root_log_dir,
-                                               f'run_{run_num}'))
+        #  run_num = io.get_run_num(root_log_dir)
+        #  log_dir = os.path.abspath(os.path.join(root_log_dir,
+                                               #  f'run_{run_num}'))
+
         io.check_else_make_dir(log_dir)
-
         self.log_dir = log_dir
         self.train_dir = os.path.join(self.log_dir, 'training')
         self.checkpoint_dir = os.path.join(self.log_dir, 'checkpoints')
