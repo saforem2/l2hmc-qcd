dab75722b69e9912a65eeced6398ef1fbd9009a1
Sam Foreman <saforem2@gmail.com>
2019-06-24 04:31:53 -0500

    Debugging `dynamics/gauge_dynamics.py`.
    Former-commit-id: 4042db9259c9f2b9d7c3fecb731248c1a66093c1

--- l2hmc-qcd/models/gauge_model.py
+++ l2hmc-qcd/models/gauge_model.py
@@ -337,7 +337,7 @@ class GaugeModel:
                                              inputs['x_proposed'],
                                              inputs['px'])
             x_loss = ls / x_std_loss - x_std_loss / ls
-            tf.add_to_collection('losses', x_std_loss)
+            #  tf.add_to_collection('losses', x_std_loss)
             tf.add_to_collection('losses', x_loss)
 
         with tf.name_scope('z_std_loss'):
@@ -345,7 +345,7 @@ class GaugeModel:
                                              inputs['z_proposed'],
                                              inputs['pz'])
             z_loss = ls / z_std_loss - z_std_loss / ls
-            tf.add_to_collection('losses', z_std_loss)
+            #  tf.add_to_collection('losses', z_std_loss)
             tf.add_to_collection('losses', z_loss)
 
         with tf.name_scope('std_loss'):
@@ -402,13 +402,13 @@ class GaugeModel:
             x_charge_loss = self._calc_charge_loss(inputs['x_init'],
                                                    inputs['x_proposed'],
                                                    inputs['px'])
-            tf.add_to_collection('losses', x_charge_loss)
+            #  tf.add_to_collection('losses', x_charge_loss)
 
         with tf.name_scope('z_charge_loss'):
             z_charge_loss = self._calc_charge_loss(inputs['z_init'],
                                                    inputs['z_proposed'],
                                                    inputs['pz'])
-            tf.add_to_collection('losses', x_charge_loss)
+            #  tf.add_to_collection('losses', x_charge_loss)
 
         with tf.name_scope('total_charge_loss'):
             charge_loss = self.charge_weight * (x_charge_loss
