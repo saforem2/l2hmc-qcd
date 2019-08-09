4fa29343cd7c94fa4be24a6e1d872d0254f0fc35
Sam Foreman <saforem2@gmail.com>
2019-08-09 18:27:21 -0500

    Renamed `variables.py` to `config.py` and added `update.py` method that sets double (tf.float64) floating poing precision project wide if `--float64` command line passed to `main.py`.
    Former-commit-id: 5b49feb35b2e1661de951c38aa6506214ddd8c50

---
+++ l2hmc-qcd/update.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import config
+
+
+def set_precision(precision):
+    """Set floating point precision project-wide."""
+    if precision == 'float64':
+        config.TF_FLOAT = tf.float64
+        config.NP_FLOAT = np.float64
+        config.TF_INT = tf.int64
+        config.NP_INT = np.int64
+    elif precision == 'float32':
+        config.TF_FLOAT = tf.float32
+        config.NP_FLOAT = np.float32
+        config.TF_INT = tf.int32
+        config.NP_INT = np.int32
