09a85bf6c6b217ed7c0adb22bbf09603bc4c6b5c
Sam Foreman <saforem2@gmail.com>
2019-08-20 19:35:10 -0500

    Removed `utils/tf_logging.py` and moved relevant functions to `loggers/summary_utils.py`.

--- l2hmc-qcd/loggers/summary_utils.py
+++ l2hmc-qcd/loggers/summary_utils.py
@@ -13,7 +13,81 @@ from __future__ import print_function
 
 import tensorflow as tf
 from lattice.lattice import u1_plaq_exact_tf
-from utils.tf_logging import variable_summaries, grad_norm_summary
+
+
+def grad_norm_summary(name_scope, grad):
+    with tf.name_scope(name_scope + '_gradients'):
+        grad_norm = tf.sqrt(tf.reduce_mean(grad ** 2))
+        summary_name = name_scope + '_grad_norm'
+        tf.summary.scalar(summary_name, grad_norm)
+
+
+def check_var_and_op(name, var):
+    return (name in var.name or name in var.op.name)
+
+
+def variable_summaries(var, name=''):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)"""
+    mean_name = 'mean'
+    stddev_name = 'stddev'
+    max_name = 'max'
+    min_name = 'min'
+    hist_name = 'histogram'
+    if name != '':
+        mean_name = name + '/' + mean_name
+        stddev_name = name + '/' + stddev_name
+        max_name = name + '/' + max_name
+        min_name = name + '/' + min_name
+        hist_name = name + '/' + hist_name
+
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar(mean_name, mean)
+    with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar(stddev_name, stddev)
+    tf.summary.scalar(max_name, tf.reduce_max(var))
+    tf.summary.scalar(min_name, tf.reduce_min(var))
+    tf.summary.histogram(hist_name, var)
+    # activation summaries
+    #  tf.summary.histogram(tensor_name + '/activations', x)
+    #  tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
+
+def add_loss_summaries(total_loss):
+    """Add summaries for losses in GaugeModel.
+
+    Generates a moving average for all losses and associated summaries for
+    visualizing the performance of the network.
+
+    Args:
+        total_loss: Total loss from model._calc_loss()
+
+    Returns:
+        loss_averages_op: Op for generating moving averages of losses.
+    """
+    # Compute the moving average of all individual losses and the total
+    # loss.
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+    # Attach a scalar summary to all individual losses and the total loss;
+    # do the same for the averaged version of the losses.
+    for l in losses + [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of
+        # the loss as the original loss name.
+        tf.summary.scalar(l.op.name, l)
+        tf.summary.scalar(l.op.name + 'moving_avg', loss_averages.average(l))
+
+    return loss_averages_op
+
+
+def make_summaries_from_collection(collection, names):
+    try:
+        for op, name in zip(tf.get_collection(collection), names):
+            variable_summaries(op, name)
+    except AttributeError:
+        pass
 
 
 def _create_training_summaries(model):
@@ -113,6 +187,7 @@ def create_summaries(model, summary_dir, training=True):
         _create_training_summaries(model)
 
     _create_obs_summaries(model)
+    _create_md_summaries(model)
 
     for grad, var in grads_and_vars:
         _create_pair_summaries(grad, var)
