82cf6cb3c0e7ba0d9aedcfe25a540ff063addea0
Sam Foreman <saforem2@gmail.com>
2019-08-20 19:38:27 -0500

    Removed unnecessary functions from `models/model.py`.

--- l2hmc-qcd/models/model.py
+++ l2hmc-qcd/models/model.py
@@ -56,60 +56,6 @@ if HAS_HOROVOD:
     import horovod.tensorflow as hvd
 
 
-def lazy_property(function):
-    attribute = '_cache_' + function.__name__
-
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute):
-            setattr(self, attribute, function(self))
-        return getattr(self, attribute)
-
-    return decorator
-
-
-def doublewrap(function):
-    """A decorator decorator.
-
-    This allows for the the decorator to be used without
-    parentheses if no arguments are provided. All arguments must be optional.
-    """
-    @functools.wraps(function)
-    def decorator(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return function(args[0])
-        else:
-            return lambda wrapee: function(wrapee, *args, **kwargs)
-
-    return decorator
-
-
-@doublewrap
-def define_scope(function, scope=None, *args, **kwargs):
-    """A decorator for functions that define TensorFlow operations.
-
-    The wrapped function will only be executed once. Subsequent calls to it
-    will directly return the result so that the operations are added to the
-    graph only once.
-
-    The operations added by the function live within a tf.variable_scope(). If
-    this decorator is used with arguments, they will be forwarded to the
-    variable scope. The scope name defaults to the name of the wrapped
-    function.
-    """
-    attribute = '_cache_' + function.__name__
-    name = scope or function.__name__
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute) or self.attribute is None:
-            with tf.name_scope(name, *args, **kwargs):
-                setattr(self, attribute, function(self))
-            return getattr(self, attribute)
-    return decorator
-
-
 def check_log_dir(log_dir):
     """Check log_dir for existing checkpoints."""
     assert os.path.isdir(log_dir)
