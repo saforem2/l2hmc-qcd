46dff598a55e7ab9cf3581b2e40d910ee0b8c52a
Sam Foreman <saforem2@gmail.com>
2019-09-17 22:43:18 -0500

    Modified the `check_reversibility` method of `GaussianMixtureModel` class to compute the sum of the squared difference between the expected and observed. Additionally, added `create_means`, `create_covs` methods.
    Former-commit-id: bbf1c826a532c387f2c91375be8fa9ccffc804b2

--- l2hmc-qcd/models/gmm_model.py
+++ l2hmc-qcd/models/gmm_model.py
@@ -22,9 +22,9 @@ from collections import namedtuple
 from utils.horovod_utils import warmup_lr  # noqa: 401
 from utils.distributions import GMM, gen_ring
 from base.base_model import BaseModel
-from .gauge_model import allclose
+#  from .gauge_model import allclose
 from dynamics.dynamics import Dynamics
-from .params import GMM_PARAMS
+from params.gmm_params import GMM_PARAMS
 from config import GLOBAL_SEED, TF_FLOAT, NP_FLOAT, HAS_HOROVOD
 
 if HAS_HOROVOD:
@@ -116,44 +116,41 @@ class GaussianMixtureModel(BaseModel):
         for key, val in self.params.items():
             setattr(self, key, val)
 
-        sigma1 = params.get('sigma1', 0.05)
-        sigma2 = params.get('sigma2', 0.05)
-        self.sigmas = (sigma1, sigma2)
-        #  self.sigma = params.get('sigma', 0.05)
         self.eps_trainable = not self.eps_fixed
-        self.num_distributions = params.get('num_distributions', 2)
+        self.build(params)
 
-        self.build()
-
-    def build(self):
+    def build(self, params=None):
         """Build TensorFlow graph."""
+        params = self.params if params is None else params
+        num_distributions = params.get('num_distributions', 2)
+
         t0 = time.time()
         io.log(80 * '-')
-        io.log(f'INFO: Building graph for `GaugeModel`...')
+        io.log(f'INFO: Building graph for `GaussianMixtureModel`...')
         with tf.name_scope('init'):
             # ***************************************************************
-            # Create distribution defining Gaussian Mixture Model
+            # Create target distribution for Gaussian Mixture Model
             # ---------------------------------------------------------------
-            means, covs, dist_arr, distribution = self._create_distribution(
-                self.sigmas, means=None
-            )
-            self.means = means
-            self.covs = covs
-            self.dist_arr = dist_arr
-            self.distribution = distribution
-
-            self.samples_init = self.distribution.get_samples(self.num_samples)
+            self.means = self._create_means(params)
+            self.sigmas, self.covs = self._create_covs(params)
+            self.pis = distribution_arr(self.x_dim, num_distributions)
+            self.distribution = GMM(self.means, self.covs, self.pis)
+            #  self.samples_init = self.distribution.get_samples(
+            #      self.batch_size
+            #  )
             # ***************************************************************
 
             # ***********************************************
             # Create inputs as `tf.placeholders`
             # -----------------------------------------------
             io.log(f'INFO: Creating input placeholders...')
-            self.inputs = self._create_inputs()
-            self.x = self.inputs.x
-            self.beta = self.inputs.beta
-            self.net_weights = self.inputs.net_weights
-            self.train_phase = self.inputs.train_phase
+            inputs = self._create_inputs()
+            self.inputs = inputs
+            self.x = inputs['x']
+            self.beta = inputs['beta']
+            nw_keys = ['scale_weight', 'transl_weight', 'transf_weight']
+            self.net_weights = [inputs[k] for k in nw_keys]
+            self.train_phase = inputs['train_phase']
             # ***********************************************
 
             # ***********************************************
@@ -171,28 +168,26 @@ class GaussianMixtureModel(BaseModel):
         # Run dynamics (i.e. augmented leapfrog) to generate new configs 
         # -------------------------------------------------------------------
         with tf.name_scope('l2hmc'):
+            args = (self.beta, self.net_weights, self.train_phase)
+            slf = self.save_lf
             with tf.name_scope('main_dynamics'):
-                x_dynamics = self._apply_transition(self.x, self.beta,
-                                                    self.net_weights,
-                                                    self.train_phase,
-                                                    save_lf=self.save_lf)
+                x_dynamics = self.dynamics.apply_transition(self.x, *args,
+                                                            save_lf=slf)
             if getattr(self, 'aux_weight', 1.) > 0:
                 with tf.name_scope('auxiliary_dynamics'):
                     self.z = tf.random_normal(tf.shape(self.x),
                                               dtype=TF_FLOAT,
                                               seed=GLOBAL_SEED,
                                               name='z')
-                    z_dynamics = self._apply_transition(self.z, self.beta,
-                                                        self.net_weights,
-                                                        self.train_phase,
-                                                        save_lf=False)
+                    z_dynamics = self.dynamics.apply_transition(self.z, *args,
+                                                                save_lf=False)
 
             self.x_out = x_dynamics['x_out']
             self.px = x_dynamics['accept_prob']
             self._parse_dynamics_output(x_dynamics)
 
             with tf.name_scope('check_reversibility'):
-                self.x_allclose, self.v_allclose = self._check_reversibility()
+                self.x_diff, self.v_diff = self._check_reversibility()
 
         with tf.name_scope('run_ops'):
             io.log(f'INFO: Building `run_ops`...')
@@ -239,49 +234,63 @@ class GaussianMixtureModel(BaseModel):
         io.log(80 * '-')
         # *******************************************************************
 
-    def _apply_transition(self, *args, **kwargs):
-        """Call `self.dynamics.apply_transition, using `x` as input."""
-        return self.dynamics.apply_transition(*args, **kwargs)
+    def _double_check(self, key, params, default_val=None):
+        """Check if key is in params, else, check if `self.key` is defined."""
+        return params.get(key, getattr(self, key, default_val))
 
-    def _check_reversibility(self):
-        x_in = tf.random_normal(self.x.shape,
-                                dtype=TF_FLOAT,
-                                seed=GLOBAL_SEED,
-                                name='x_reverse_check')
-        v_in = tf.random_normal(self.x.shape,
-                                dtype=TF_FLOAT,
-                                seed=GLOBAL_SEED,
-                                name='v_reverse_check')
+    def _create_means(self, params=None):
+        """Create means of target distribution."""
+        params = self.params if params is None else params
+        diag = self._double_check('diag', params, False)
 
-        dynamics_check = self.dynamics._check_reversibility(x_in, v_in,
-                                                            self.beta,
-                                                            self.net_weights,
-                                                            self.train_phase)
-        xb = dynamics_check['xb']
-        vb = dynamics_check['vb']
+        means = np.zeros((self.x_dim, self.x_dim), dtype=NP_FLOAT)
+        if diag:
+            for i in range(self.x_dim):
+                means[i::self.x_dim, i] = self.center
+        else:
+            means = np.zeros((self.x_dim, self.x_dim))
+            means[::2, 0] = self.center
+            means[1::2, 0] = - self.center
 
-        x_allclose = allclose(x_in, xb)  # xb = backward(forward(x_in))
-        v_allclose = allclose(v_in, vb)
+        return means.astype(NP_FLOAT)
 
-        return x_allclose, v_allclose
+    def _create_covs(self, params=None):
+        """Create covariance matrix from of individual covariance matrices."""
+        params = self.params if params is None else params
+        sigmas = self._double_check('sigmas', params, None)
+        if sigmas is None:
+            sigma = self._double_check('sigma', params, None)
+            if sigma is not None:
+                sigmas = sigma * np.ones(self.x_dim)
+            else:
+                sigma1 = self._double_check('sigma1', params, 0.1)
+                sigma2 = self._double_check('sigma2', params, 0.1)
+                sigmas = np.array([sigma1, sigma2])
+
+        sigmas = np.array(sigmas, dtype=NP_FLOAT)
+        covs = np.array(
+            [s * np.eye(self.x_dim) for s in sigmas], dtype=NP_FLOAT
+        )
+
+        return sigmas, covs
 
     def _create_distribution(self, sigmas, means=None):
         """Initialize distribution using utils/distributions.py."""
         diag = getattr(self, 'diag', False)
 
         if means is None:
-            if self.centers is None:
-                centers = 1.
+            if self.center is None:
+                center = 1.
             else:
-                centers = self.centers
+                center = self.center
 
             means = np.zeros((self.x_dim, self.x_dim), dtype=NP_FLOAT)
             if diag:
                 for i in range(self.x_dim):
-                    means[i::self.x_dim, i] = centers
+                    means[i::self.x_dim, i] = center
             else:
-                means[::2, 0] = centers
-                means[1::2, 0] = -centers
+                means[::2, 0] = center
+                means[1::2, 0] = -center
         else:
             means = np.array(means).astype(NP_FLOAT)
 
@@ -318,8 +327,8 @@ class GaussianMixtureModel(BaseModel):
                 'eps_trainable': not self.eps_fixed,
                 'num_filters': 2,
                 'x_dim': self.x_dim,
-                'batch_size': self.num_samples,
-                '_input_shape': (self.num_samples, self.x_dim),
+                'batch_size': self.batch_size,
+                '_input_shape': (self.batch_size, self.x_dim),
             })
 
             dynamics_params.update(params)
@@ -342,13 +351,7 @@ class GaussianMixtureModel(BaseModel):
 
     def gaussian_loss(self, x_data, z_data):
         """Calculate the Gaussian loss and return op. for calculating it."""
-        #  eps = getattr(self, 'eps', 0.2)
-        #  md_dist = eps * self.num_steps
-
-        loss = self._gaussian_loss(x_data, z_data, mean=0., sigma=1.)
-
-        return loss
-
+        return self._gaussian_loss(x_data, z_data, mean=0., sigma=1.)
 
     def _parse_dynamics_output(self, dynamics_output):
         """Parse output dictionary from `self.dynamics.apply_transition."""
@@ -371,51 +374,6 @@ class GaussianMixtureModel(BaseModel):
                 'l2hmc_fns_f': self._extract_l2hmc_fns(self.fns_out_f),
                 'l2hmc_fns_b': self._extract_l2hmc_fns(self.fns_out_b),
             }
-
-    def _create_inputs(self):
-        """Create input paceholders (if not executing eagerly).
-        Returns:
-            outputs: Dictionary with the following entries:
-                x: Placeholder for input lattice configuration with
-                    shape = (batch_size, x_dim) where x_dim is the number of
-                    links on the lattice and is equal to lattice.time_size *
-                    lattice.space_size * lattice.dim.
-                beta: Placeholder for inverse coupling constant.
-                charge_weight: Placeholder for the charge_weight (i.e. alpha_Q,
-                    the multiplicative factor that scales the topological
-                    charge term in the modified loss function) .
-                net_weights: Array of placeholders, each of which is a
-                    multiplicative constant used to scale the effects of the
-                    various S, Q, and T functions from the original paper.
-                    net_weights[0] = 'scale_weight', multiplies the S fn.
-                    net_weights[1] = 'transformation_weight', multiplies the Q
-                    fn.  net_weights[2] = 'translation_weight', multiplies the
-                    T fn.
-                train_phase: Boolean placeholder indicating if the model is
-                    curerntly being trained. 
-        """
-        def scalar_ph(name, dtype=TF_FLOAT):
-            return tf.placeholder(dtype=dtype, shape=(), name=name)
-
-        with tf.name_scope('inputs'):
-            if not tf.executing_eagerly():
-                x = tf.placeholder(dtype=TF_FLOAT,
-                                   shape=(self.batch_size, self.x_dim),
-                                   name='x')
-                beta = scalar_ph('beta')
-                train_phase = scalar_ph('is_training', dtype=tf.bool)
-                scale_weight = scalar_ph('scale_weight')
-                transl_weight = scalar_ph('translation_weight')
-                transf_weight = scalar_ph('transformation_weight')
-                net_weights = [scale_weight, transl_weight, transf_weight]
-
-            Inputs = namedtuple('Inputs',
-                                ['x', 'beta', 'net_weights', 'train_phase'])
-            inputs = Inputs(x, beta, net_weights, train_phase)
-
-            return inputs
-
-        return inputs
 
     def _build_run_ops(self):
         """Build `run_ops` used for running inference w/ trained model."""
