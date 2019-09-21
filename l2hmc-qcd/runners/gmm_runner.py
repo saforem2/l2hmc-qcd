b0546b835a1815d53cd35361486892f41f775889
Sam Foreman <saforem2@gmail.com>
2019-09-20 21:57:11 -0500

    Updates to `GaussianMixtureModelRunner` in `runners/gmm_runner.py`.

--- l2hmc-qcd/runners/gmm_runner.py
+++ l2hmc-qcd/runners/gmm_runner.py
@@ -24,7 +24,11 @@ class GaussianMixtureModelRunner:
 
         else:
             self.inputs_dict = RunLogger.build_inputs_dict(inputs)
-            self.run_ops_dict = self.logger.build_run_ops_dict(params, run_ops)
+            model_type = 'gmm_model'
+            self.run_ops_dict = self.logger.build_run_ops_dict(params,
+                                                               run_ops,
+                                                               model_type)
+
 
         #  if self.params['eps'] is None:
         self.eps = self.sess.run(self.run_ops_dict['dynamics_eps'])
@@ -94,7 +98,6 @@ class GaussianMixtureModelRunner:
 
     def run(self, **kwargs):
         """Run inference using the trained sampler."""
-
         run_steps = int(kwargs.get('run_steps', 5000))
         beta = kwargs.get('beta_final', self.params.get('beta_final', 1))
         net_weights = kwargs.get('net_weights', [1., 1., 1.])
