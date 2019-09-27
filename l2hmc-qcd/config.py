a814e785355d134e86cae32eb93214dbf193e425
Sam Foreman <saforem2@gmail.com>
2019-09-26 21:54:13 -0500

    Added `noqa` flags in `config.py` to ignore unnecessary imports when running linters.
    Former-commit-id: 1691b6bc27aff6499b2e1af74aab583d9ea72cf1

--- l2hmc-qcd/config.py
+++ l2hmc-qcd/config.py
@@ -7,32 +7,33 @@ import os
 import tensorflow as tf
 import numpy as np
 
+
 try:
-    import memory_profiler
+    import memory_profiler  # noqa: F401
     HAS_MEMORY_PROFILER = True
 except ImportError:
     HAS_MEMORY_PROFILER = False
 
 try:
-    import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt  # noqa: F401
     HAS_MATPLOTLIB = True
 except ImportError:
     HAS_MATPLOTLIB = False
 
 try:
-    import horovod.tensorflow as hvd
+    import horovod.tensorflow as hvd  # noqa: F401
     HAS_HOROVOD = True
 except ImportError:
     HAS_HOROVOD = False
 
 try:
-    import psutil
+    import psutil  # noqa: F401
     HAS_PSUTIL = True
 except ImportError:
     HAS_PSUTIL = False
 
 try:
-    from comet_ml import Experiment
+    from comet_ml import Experi  # noqa: F401ment
     HAS_COMET = True
 except ImportError:
     HAS_COMET = False
@@ -55,7 +56,7 @@ COLORS = 5000 * ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
 MARKERS = 5000 * ['o', 's', 'x', 'v', 'h', '^', 'p', '<', 'd', '>', 'o']
 LINESTYLES = 5000 * ['-', '--', ':', '-.', '-', '--', ':', '-.', '-', '--']
 
-GLOBAL_SEED = 42
+GLOBAL_SEED = 9
 
 header = ("{:^12s}" + 8 * "{:^10s}").format(
     "STEP", "t/STEP", "% ACC", "EPS", "BETA",
