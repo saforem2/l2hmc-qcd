4c76c849ed928be94bcbb2baea77ee7871dd6ef9
Sam Foreman <saforem2@gmail.com>
2020-05-01 14:39:39 -0500

    Updates to `args/*`
    Former-commit-id: b1a8be7220c6341eb8ebfa8ef0af99d9aa9fae14

--- l2hmc-qcd/args/l2hmc.sh
+++ l2hmc-qcd/args/l2hmc.sh
@@ -3,4 +3,26 @@
 RUNNER_NP='../gauge_inference_np.py'
 ARGS='./l2hmc.txt'
 
-python3 ${RUNNER_NP} @${ARGS}
+export KMP_AFFINITY=granularity=fine,compact,1,0
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=8
+export KMP_SETTINGS=TRUE
+
+# python3 ${RUNNER_NP} @${ARGS}
+B="--batch_size 3"
+P="--print_steps 10"
+R="--run_steps 50000"
+# ARGS=${BS} ${PS} ${RS}
+
+# ARGS = "--batch_size 1 \
+#         --print_steps 10 \
+#         --run_steps 50000"
+
+
+#python3 ${RUNNER_NP} ${P} ${R} --batch_size 16 --beta 4.
+# python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 4. --mix_samplers
+python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 5. --mix_samplers
+python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 6. --mix_samplers
+python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 7. --mix_samplers
+
+#python3 ${RUNNER_NP} ${P} ${R} --batch_size 16 --beta 5. --mix_samplers
