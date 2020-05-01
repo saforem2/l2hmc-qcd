b1a8be7220c6341eb8ebfa8ef0af99d9aa9fae14
Sam Foreman <saforem2@gmail.com>
2020-05-01 14:39:39 -0500

    Updates to `args/*`

--- l2hmc-qcd/args/hmc.sh
+++ l2hmc-qcd/args/hmc.sh
@@ -3,4 +3,18 @@
 RUNNER_NP='../gauge_inference_np.py'
 ARGS='./hmc.txt'
 
-python3 ${RUNNER_NP} @${ARGS}
+export KMP_AFFINITY=granularity=fine,compact,1,0
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=8
+export KMP_SETTINGS=TRUE
+
+# python3 ${RUNNER_NP} @${ARGS}
+
+B="--batch_size 1"
+P="--print_steps 10"
+R="--run_steps 20000"
+
+python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 5. --mix_samplers
+python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 6. --mix_samplers
+python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 7. --mix_samplers
+
--- l2hmc-qcd/args/train.sh
+++ l2hmc-qcd/args/train.sh
@@ -3,4 +3,9 @@
 TRAINER='../main.py'
 ARGS='./gauge_args.txt'
 
+export KMP_AFFINITY=granularity=fine,compact,1,0
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=8
+export KMP_SETTINGS=TRUE
+
 python3 ${TRAINER} @${ARGS}
