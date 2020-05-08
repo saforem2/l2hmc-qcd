bebffcd99d27c95cdf7179e15e48b7c307eed9af
Sam Foreman <saforem2@gmail.com>
2020-05-08 13:32:27 -0500

    Updates to `args/*`
    Former-commit-id: fd8c21dd3fe434cbf7e35766da415e244b0c2f40

--- l2hmc-qcd/args/hmc.sh
+++ l2hmc-qcd/args/hmc.sh
@@ -8,13 +8,13 @@ export KMP_BLOCKTIME=1
 export OMP_NUM_THREADS=8
 export KMP_SETTINGS=TRUE
 
-# python3 ${RUNNER_NP} @${ARGS}
+python3 ${RUNNER_NP} @${ARGS}
 
-B="--batch_size 1"
-P="--print_steps 10"
-R="--run_steps 20000"
-
-python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 5. --mix_samplers
-python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 6. --mix_samplers
-python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 7. --mix_samplers
+# B="--batch_size 2"
+# P="--print_steps 100"
+# R="--run_steps 20000"
+#
+# python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 5. --mix_samplers
+#python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 6. --mix_samplers
+#python3 ${RUNNER_NP} ${B} ${P} ${R} --hmc --beta 7. --mix_samplers
 
--- l2hmc-qcd/args/l2hmc.sh
+++ l2hmc-qcd/args/l2hmc.sh
@@ -8,11 +8,8 @@ export KMP_BLOCKTIME=1
 export OMP_NUM_THREADS=8
 export KMP_SETTINGS=TRUE
 
-# python3 ${RUNNER_NP} @${ARGS}
-B="--batch_size 3"
-P="--print_steps 10"
-R="--run_steps 50000"
-# ARGS=${BS} ${PS} ${RS}
+python3 ${RUNNER_NP} @${ARGS}
+
 
 # ARGS = "--batch_size 1 \
 #         --print_steps 10 \
@@ -20,9 +17,9 @@ R="--run_steps 50000"
 
 
 #python3 ${RUNNER_NP} ${P} ${R} --batch_size 16 --beta 4.
-# python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 4. --mix_samplers
-python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 5. --mix_samplers
-python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 6. --mix_samplers
-python3 ${RUNNER_NP} ${P} ${R} ${B} --beta 7. --mix_samplers
+# python3 ${RUNNER_NP} ${P} ${R} ${M} ${NW} --beta 5. --mix_samplers
+# python3 ${RUNNER_NP} ${ARGS} ${NW} --mix_samplers --beta 5.
+# python3 ${RUNNER_NP} ${P} ${R} ${M} --beta 6. --mix_samplers
+# python3 ${RUNNER_NP} ${P} ${R} ${M} --beta 7. --mix_samplers
 
 #python3 ${RUNNER_NP} ${P} ${R} --batch_size 16 --beta 5. --mix_samplers
