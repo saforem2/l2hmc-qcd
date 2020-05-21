e08f06d13d642579e46133fc4b69dc6359d9e70c
Sam Foreman <saforem2@gmail.com>
2020-05-21 01:33:26 -0500

    Minor changes to `args/tf_inference.sh`
    Former-commit-id: 67266bf35bb9bf8d6f1850a7f8ed53e800739c98

--- l2hmc-qcd/args/tf_inference.sh
+++ l2hmc-qcd/args/tf_inference.sh
@@ -7,8 +7,8 @@ export KMP_BLOCKTIME=1
 export OMP_NUM_THREADS=8
 export KMP_SETTINGS=TRUE
 
-ARGS='--run_steps 100 --plot_chains 5 --print_steps 50'
-HMC_ARGS='--run_steps 5000 --plot_chains 10 --print_steps 10 --hmc'
+ARGS='--run_steps 10000 --plot_chains 5 --print_steps 50'
+HMC_ARGS='--run_steps 10000 --plot_chains 10 --print_steps 50 --hmc'
 LOG_DIR='../../gauge_logs/gce_logs/2020_05_14/L16_b1024_lf2_GaugeNetwork_aw00_qw01_pw01_dp05_f32_0203/'
 
 betas=(
@@ -21,7 +21,7 @@ betas=(
 for beta in ${betas[*]}
 do
     python3 ${RUNNER} --beta $beta --log_dir ${LOG_DIR} ${ARGS}
-    # python3 ${RUNNER} --beta $beta --log_dir ${LOG_DIR} ${HMC_ARGS}
+    python3 ${RUNNER} --beta $beta --log_dir ${LOG_DIR} ${HMC_ARGS}
     # python3 ${RUNNER} --beta $beta --log_dir ${LOG_DIR} ${HMC_ARGS} --eps 0.1
     # python3 ${RUNNER} --beta $beta --log_dir ${LOG_DIR} ${HMC_ARGS} --eps 0.2
 done
