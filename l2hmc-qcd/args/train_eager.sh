bb7df3ffeaef9ce9261edf18b4b3e2d4d04c6424
Sam Foreman <saforem2@gmail.com>
2020-06-10 12:04:03 -0500

    Adds `args/train_eager.sh`
    Former-commit-id: c08ea28805b69256cb7a2fba5683ac0e95e0e399

---
+++ l2hmc-qcd/args/train_eager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TRAINER='../main_eager.py'
+ARGS='./gauge_args_eager.txt'
+
+KMP_BLOCKTIME=0
+OMP_NUM_THREADS=16
+KMP_SETTINGS=TRUE
+KMP_AFFINITY=granularity=fine,compact,1,0
+
+# export TF_XLA_FLAGS=“--tf_xla_cpu_global_jit”
+
+ TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
+
+ipython3 -m pudb ${TRAINER} @${ARGS}
