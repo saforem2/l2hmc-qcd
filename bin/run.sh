1f52146563460ab63dcbcb1b381fbe2a117b3cf5
Sam Foreman <saforem2@gmail.com>
2020-06-17 16:53:48 -0500

    Adds `run.py` for running inference on trained models`

---
+++ bin/run.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+RUNNER='../l2hmc-qcd/run.py'
+ARGS='./inference_args.txt'
+
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=16
+export KMP_SETTINGS=TRUE
+export KMP_AFFINITY='granularity=fine,verbose,compact,1,0'
+
+# export TF_XLA_FLAGS=“--tf_xla_cpu_global_jit”
+
+export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
+
+
+ipython3 -m pudb ${RUNNER} @${ARGS}
