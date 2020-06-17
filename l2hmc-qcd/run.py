55cde9243fe7985328540bb9524d89179d8d35b9
Sam Foreman <saforem2@gmail.com>
2020-06-17 16:54:47 -0500

    Renames `utils/parse_inference_args_new.py` to `utils/parse_inference_args.py`
    Former-commit-id: 3a2e14d4b456ec24984baabf54d379b3d9bc4980

--- l2hmc-qcd/run.py
+++ l2hmc-qcd/run.py
@@ -6,7 +6,7 @@ Run inference on trained model.
 from __future__ import absolute_import, division, print_function
 
 from utils.attr_dict import AttrDict
-from utils.parse_inference_args_new import parse_args
+from utils.parse_inference_args import parse_args
 from utils.inference_utils import load_and_run
 
 
