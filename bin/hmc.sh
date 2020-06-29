b3c72f7feb28d9f78764fabdf627401380010b5c
Sam Foreman <saforem2@gmail.com>
2020-06-29 15:42:56 -0500

    Adds `bin/hmc.sh`
    Former-commit-id: 8104917686e374d767803df2cbb8f6b1ed01c828

---
+++ bin/hmc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=16
+export KMP_SETTINGS=TRUE
+export KMP_AFFINITY='granularity=fine,verbose,compact,1,0'
+
+# export TF_XLA_FLAGS=“--tf_xla_cpu_global_jit”
+
+export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
+
+RUNNER='../l2hmc-qcd/run.py'
+
+RUN_STEPS=2000
+LATTICE_SHAPE="128, 16, 16, 2"
+
+NUM_STEPS=( 1 2 3 4 5 8 10 )
+EPS_ARR=( 0.075 0.1 0.15 )
+BETA_ARR=(4. 4.5 5 )
+
+for beta in ${BETA_ARR[@]}
+do
+    for eps in ${EPS_ARR[@]}
+    do
+        for steps in ${NUM_STEPS[@]}
+        do
+            echo -e "\n"
+            python3 ${RUNNER} \
+                --hmc \
+                --eps ${eps} \
+                --beta ${beta} \
+                --num_steps ${steps} \
+                --run_steps ${RUN_STEPS} \
+                --lattice_shape '128, 16, 16, 2'
+            echo -e "\n"
+        done
+    done
+done
