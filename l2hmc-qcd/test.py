275718f30f13bf925d4a98bc1827f68126f3bd68
Sam Foreman <saforem2@gmail.com>
2020-07-06 21:46:44 -0500

    Adds test for resuming training to `test.py`

--- l2hmc-qcd/test.py
+++ l2hmc-qcd/test.py
@@ -9,7 +9,7 @@ import os
 import sys
 import argparse
 
-from config import DEFAULT_FLAGS, PROJECT_DIR
+from config import PROJECT_DIR
 from utils.attr_dict import AttrDict
 from utils.training_utils import train
 from utils.inference_utils import load_and_run, run, run_hmc
@@ -17,6 +17,39 @@ from utils.inference_utils import load_and_run, run, run_hmc
 DESCRIPTION = (
     "Various test functions to make sure everything runs as expected."
 )
+
+TEST_FLAGS = AttrDict({
+    'log_dir': None,
+    'restore': False,
+    'inference': True,
+    'run_steps': 100,
+    'horovod': False,
+    'rand': True,
+    'eps': 0.1,
+    'num_steps': 2,
+    'hmc': False,
+    'eps_fixed': False,
+    'beta_init': 1.,
+    'beta_final': 1.,
+    'train_steps': 25,
+    'save_steps': 5,
+    'print_steps': 1,
+    'logging_steps': 1,
+    'hmc_start': True,
+    'hmc_steps': 25,
+    'dropout_prob': 0.1,
+    'warmup_steps': 10,
+    'lr_init': 0.001,
+    'lr_decay_rate': 0.96,
+    'lr_decay_steps': 1000,
+    'plaq_weight': 10.,
+    'charge_weight': 0.1,
+    'save_train_data': True,
+    'separate_networks': False,
+    'network_type': 'GaugeNetwork',
+    'lattice_shape': [128, 16, 16, 2],
+    'units': [512, 256, 256, 256, 512],
+})
 
 
 def parse_args():
@@ -108,30 +141,52 @@ def test_separate_networks(flags):
     }
 
 
+def test_resume_training(flags):
+    """Test restoring a training session from a checkpoint."""
+    assert flags.log_dir is not None
+    flags.beta_init, flags.beta_final = flags.beta_final, flags.beta_final + 1
+
+    x, model, train_data, flags = train(flags)
+    model, run_data = run(model, flags, x=x)
+
+    return {
+        'x': x,
+        'flags': flags,
+        'model': model,
+        'run_data': run_data,
+        'train_data': train_data,
+    }
+
+
 def test(flags):
     """Run tests."""
+    hmc_out = test_hmc_run()
+    single_net_out = test_single_network(flags)
+    separate_nets_out = test_separate_networks(flags)
+    restored_out = test_resume_training(single_net_out['flags'])
+
     return {
-        'single_network': test_single_network(flags),
-        'separate_networks': test_separate_networks(flags),
-        'hmc': test_hmc_run(),
+        'hmc': hmc_out,
+        'single_network': single_net_out,
+        'separate_networks': separate_nets_out,
+        'restored': restored_out,
     }
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        _ = test(DEFAULT_FLAGS)
+        _ = test(TEST_FLAGS)
     else:
         FLAGS = parse_args()
-        FLAGS = AttrDict(FLAGS.__dict__)
 
         if FLAGS.test_hmc_run:
             _ = test_hmc_run()
 
         if FLAGS.test_separate_networks:
-            _ = test_separate_networks(DEFAULT_FLAGS)
+            _ = test_separate_networks(TEST_FLAGS)
 
         if FLAGS.test_single_network:
-            _ = test_single_network(FLAGS)
+            _ = test_single_network(TEST_FLAGS)
 
         if FLAGS.test_inference_from_model:
             if FLAGS.log_dir is None:
