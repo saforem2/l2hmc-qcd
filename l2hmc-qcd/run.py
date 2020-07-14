21de671615a60c4ba97d6d1d312827b87ddc4001
Sam Foreman <saforem2@gmail.com>
2020-07-14 20:56:10 +0000

    Fixes bug in inference when `--eps` not specified

--- l2hmc-qcd/run.py
+++ l2hmc-qcd/run.py
@@ -25,9 +25,30 @@ def main(args):
         return
 
     hmc_dir = os.path.join(args.log_dir, 'inference_hmc')
+    train_flags_file = os.path.join(args.log_dir, 'training', 'FLAGS.z')
+    train_flags = io.loadz(train_flags_file)
+    if args.eps is None:
+        eps_file = os.path.join(args.log_dir, 'training',
+                                'train_data', 'eps.z')
+        eps_arr = io.loadz(eps_file)
+        args.eps = eps_arr[-1]
+
+    if args.beta is None:
+        args.beta = train_flags['beta_final']
+
+    args.update({
+        'num_steps': int(train_flags['num_steps']),
+        'lattice_shape': train_flags['lattice_shape'],
+    })
 
     _, _, x_hmc = run_hmc(args=args, hmc_dir=hmc_dir, skip_existing=skip)
+
+    args.hmc = False
     _, _, _ = load_and_run(args, x=x_hmc)
+
+    x_train = io.loadz(os.path.join(args.log_dir, 'training',
+                                    'train_data', 'x_rank0.z'))
+    _, _, _ = load_and_run(args, x=x_train)
 
     io.log(80 * '=')
     return
