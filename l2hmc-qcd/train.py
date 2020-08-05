31f3282fbcfedf37cd5fdea95175a2ce0d87221b
Sam Foreman <saforem2@gmail.com>
2020-08-05 13:42:37 -0500

    Moves `hvd.init()` calls around
    Former-commit-id: ff10050de4d597bf9555368d5b93e2a7f6e897a1

--- l2hmc-qcd/train.py
+++ l2hmc-qcd/train.py
@@ -11,6 +11,7 @@ import logging
 
 import tensorflow as tf
 import horovod.tensorflow as hvd
+
 import utils.file_io as io
 
 from utils import DummyTqdmFile
@@ -24,9 +25,12 @@ def main(args, log_file=None):
     """Main method for training."""
     #  io.log(TRAIN_STR)
     tf.keras.backend.set_floatx('float32')
-    x, dynamics, train_data, args = train(args, log_file=log_file, md_steps=10)
-    if args.run_steps > 0:
-        dynamics, run_data, x = run(dynamics, args, x=x)
+    x, dynamics, train_data, args = train(args,
+                                          rank=RANK,
+                                          md_steps=10,
+                                          log_file=log_file)
+    if args.run_steps > 0 and (RANK == 0):
+        #  dynamics, run_data, x = run(dynamics, args, x=x)
         # run again with random start
         dynamics, run_data, x = run(dynamics, args)
 
@@ -40,7 +44,11 @@ if __name__ == '__main__':
         tf.config.experimental.set_memory_growth(gpu, True)
     if GPUS:
         GPU = GPUS[hvd.local_rank()]
-        tf.config.experimental.set_visible_devices(gpu, 'GPU')
+        tf.config.experimental.set_visible_devices(GPU, 'GPU')
+
+    RANK = hvd.rank()
+    IS_CHIEF = (RANK == 0)
+    io.log(f'Number of devices: {hvd.size()}', RANK)
 
     FLAGS = parse_args()
     FLAGS = AttrDict(FLAGS.__dict__)
