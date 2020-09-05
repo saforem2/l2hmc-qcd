2dc63dfeae6fa9c9050c24bd60bfc61b3cde97d4
Sam Foreman <saforem2@gmail.com>
2020-09-04 20:18:34 -0500

    Adds automatic mixed precision when running on GPU

--- l2hmc-qcd/utils/__init__.py
+++ l2hmc-qcd/utils/__init__.py
@@ -9,12 +9,15 @@ Date: 08/26/2020
 from tqdm.auto import tqdm
 import tensorflow as tf
 import horovod.tensorflow as hvd  # pylint:disable=wrong-import-order
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 hvd.init()
 GPUS = tf.config.experimental.list_physical_devices('GPU')
 for gpu in GPUS:
     tf.config.experimental.set_memory_growth(gpu, True)
 if GPUS:
     tf.config.experimental.set_visible_devices(GPUS[hvd.local_rank()], 'GPU')
+    policy = mixed_precision.Policy('mixed_float16')
+    mixed_precision.set_policy(policy)
 
 
 class DummyTqdmFile:
