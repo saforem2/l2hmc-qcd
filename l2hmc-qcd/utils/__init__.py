26eae1447d328f02354df81dbb3b74d5395862ed
Sam Foreman <saforem2@gmail.com>
2020-12-29 17:30:07 -0600

    Updates to `SKEYS` in `utils/__init__.py`
    Former-commit-id: 21d74a7aa5f12db510791e81ed49c91fb8b47619

--- l2hmc-qcd/utils/__init__.py
+++ l2hmc-qcd/utils/__init__.py
@@ -52,25 +52,43 @@ except (ImportError, ModuleNotFoundError):
     SIZE = LOCAL_SIZE = 1
     IS_CHIEF = (RANK == 0)
 
-_d = ('f', 'b')
+_d = ('f', 'b', 'forward', 'backward')
 _t = ('start', 'mid', 'end')
+
+bad = ['sumlogdet' 'sumlogdetf', 'sumlogdetf_start', 'sumlogdetf_mid',
+       'sumlogdetf_end']
 
 
 BASE_KEYS = [
-    'H', 'Hw', 'ld', 'sld',
-    'sinQ', 'intQ', 'plaqs',
-    'xeps', 'veps', 'accept_prob',
+    'H', 'ld', 'sld',
+    'sinQ', 'intQ', # 'plaqs',
+    'xeps', 'veps', # 'accept_prob',
     'logdet', 'logdets', 'sumlogdet',
+    'accept_mask', 'xeps', 'veps', 'charges',
+    'accept_probf', 'accept_probb',
+    'plaqsf', 'plaqsb',
+]
+
+DIR_KEYS = [
+    'accept_prob', 'plaqs', 'H',  # , 'Hw',
+    'xeps', 'veps', 'logdet', 'logdets', 'sumlogdet',
+    'accept_mask', 'charges'
 ]
 
 SKEYS = [
     'forward', 'backward', 'sumlogdet_prop',
 ]
 for bk in BASE_KEYS:
+    SKEYS.append(f'{bk}')
+    #  for dk in DIR_KEYS:
+    #      for sk in SKEYS:
+    #          SKEYS.append(f'{dk}{sk}')
     for d in _d:                          # =====
         SKEYS.append(f'{bk}{d}')          # 'Hf', 'Hb', 'Hwf', 'Hwb', etc.
         for t in _t:                      # ====
             SKEYS.append(f'{bk}{d}_{t}')  # 'Hf_start', 'Hf_mid', etc.
+
+
 
 
 # pylint:disable=missing-function-docstring,unused-argument
