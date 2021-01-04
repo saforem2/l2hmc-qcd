d2f89dc70a267135ec1e9d7d9013f3af35b05520
Sam Foreman <saforem2@gmail.com>
2021-01-03 21:09:55 -0600

    Fixes bug caused by incorrect metrics in `dynamics.GaugeDynamics` (3)
    Former-commit-id: 0d5e0e37966b3af97583e413d191956682356a6f

--- l2hmc-qcd/hmc.py
+++ l2hmc-qcd/hmc.py
@@ -87,7 +87,6 @@ def multiple_runs(flags, json_file=None):
     ]
 
     num_steps = [10, 15, 20, 25]
-    run_steps = 5000
     betas = [5.0, 6.0, 7.0]
     eps = [0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
     #  eps = [0.1, 0.125, 0.15, 0.175, 0.2]
@@ -147,6 +146,6 @@ if __name__ == '__main__':
     FLAGS = parse_args()
     FLAGS = AttrDict(FLAGS.__dict__)  # pylint:disable=protected-access
     if FLAGS.run_loop:
-        multiple_runs(FLAGS.json_file)
+        multiple_runs(FLAGS, FLAGS.json_file)
     else:
         _ = main(FLAGS, FLAGS.json_file)
