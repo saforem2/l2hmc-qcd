c67b4a50feb6963bd731c91d7e53eb34cc1d6fb6
Sam Foreman <saforem2@gmail.com>
2021-06-28 11:54:51 -0500

    Updates to `utils/logger.py`
    Former-commit-id: f47f1aa8fabe24724103b00dbd5071ffec9ed6b1

--- l2hmc-qcd/utils/logger.py
+++ l2hmc-qcd/utils/logger.py
@@ -1,41 +1,22 @@
 """
 logger.py
-
-Contains implementation of `Logger` object for printing metrics.
 """
-from __future__ import absolute_import, division, print_function, annotations
-import shutil
-import datetime
-import numpy as np
+from __future__ import absolute_import, print_function, division
+import os
+
+from pathlib import Path
+from typing import Any, Union
 import tensorflow as tf
 
-try:
-    import horovod
-    import horovod.tensorflow as hvd
-    try:
-        RANK = hvd.rank()
-    except AttributeError:
-        hvd.init()
-
-    RANK = hvd.rank()
-except (ImportError, ModuleNotFoundError):
-    RANK = 0
-
-
-WIDTH, _ = shutil.get_terminal_size(fallback=(156, 50))
-
-def get_timestamp(fstr=None):
-    """Get formatted timestamp."""
-    now = datetime.datetime.now()
-    if fstr is None:
-        return now.strftime('%Y-%m-%d-%H%M%S')
-    return now.strftime(fstr)
+import numpy as np
+import joblib
+import datetime
+from utils.attr_dict import AttrDict
 
 
 def in_notebook():
-    """Check if we're currently in a jupyter notebook."""
+    """Simple checker function to see if we're currently in a notebook."""
     try:
-        # pylint:disable=import-outside-toplevel
         from IPython import get_ipython
         try:
             if 'IPKernelApp' not in get_ipython().config:
@@ -47,115 +28,139 @@ def in_notebook():
     return True
 
 
-def strformat(k, v, window: int = 0):
-    if v is None:
-        v = 'None'
+def get_timestamp(fstr: str = None):
+    """Get formatted timestamp."""
+    now = datetime.datetime.now()
+    if fstr is None:
+        return now.strftime('%Y-%m-%d-%H%M%S')
+    return now.strftime(fstr)
 
-    outstr = ''
+
+#  TensorList = "list[tf.Tensor]"
+#  TensorTuple = "tuple[tf.Tensor]"
+#  TensorArrayLike = "Union[TensorList, TensorTuple, torch.Tensor]"
+#  Scalar = "Union[int, bool, float]"
+
+TensorList = list[tf.Tensor]
+TensorTuple = tuple[tf.Tensor]
+Scalar = Union[int, bool, float]
+TensorArrayLike = Union[TensorList, TensorTuple, tf.Tensor]
+
+
+def strformat(
+        k: str,
+        v: Union[Scalar, TensorArrayLike],
+        window: int = 0
+):
+    if isinstance(v, tuple) and len(v) == 1:
+        v = v[0]
+
+    if isinstance(v, tf.Tensor):
+        v = v.numpy()
+    #  if torch.is_tensor(v):
+    #      v = v.detach().cpu().numpy()  # torch.Tensor
+
+    if isinstance(v, int):
+        return f'{str(k)}={int(v)}'
+
     if isinstance(v, bool):
-        v = 'True' if v else 'False'
-        return f'{str(k)}={v}'
+        return f'{str(k)}=True' if v else f'{str(k)}=False'
 
-    if isinstance(v, dict):
-        outstr_arr = []
-        for key, val in v.items():
-            outstr_arr.append(strformat(key, val, window))
-        outstr = '\n'.join(outstr_arr)
-    else:
-        #  if isinstance(v, (tuple, list, np.ndarray)):
-        if isinstance(v, (tuple, list, np.ndarray, tf.Tensor)):
-            v = np.array(v)
-            if window > 0 and len(v.shape) > 0:
-                window = min((v.shape[0], window))
-                avgd = np.mean(v[-window:])
-            else:
-                avgd = np.mean(v)
-            outstr = f'{str(k)}={avgd:<5.4g}'
+    if isinstance(v, float):
+        return f'{str(k)}={v:<4.3f}'
+
+    if isinstance(v, (list, np.ndarray)):
+        v = np.array(v)
+        if window > 0 and len(v.shape) > 0:
+            window = min((v.shape[0], window))
+            avgd = v[-window:].mean()
         else:
-            if isinstance(v, float):
-                outstr = f'{str(k)}={v:<5.4g}'
-            else:
-                try:
-                    outstr = f'{str(k)}={v:<5g}'
-                except ValueError:
-                    outstr = f'{str(k)}={v:<5}'
-    return outstr
+            avgd = v.mean()
+
+        return f'{str(k)}={avgd:<4.3f}'
+
+    try:
+        return f'{str(k)}={v:<3g}'
+    except ValueError:
+        return f'{str(k)}={v:<3}'
 
 
 
-
-# noqa:E999
 class Console:
-    """Fallback console object used in case `rich` isn't installed."""
+    """Fallback console object used as in case `rich` isn't installed."""
+    def rule(self, s, *args, **kwargs):
+        line = len(s) * '-'
+        self.log('\n'.join([line, s, line]), *args, **kwargs)
+
     @staticmethod
     def log(s, *args, **kwargs):
-        if RANK == 0:
-            now = get_timestamp('%X')
-            print(f'[{now}] {s}', *args, **kwargs)
+        now = get_timestamp('%X')
+        print(f'[{now}]  {s}', *args, **kwargs)
+
 
 
 class Logger:
     """Logger class for pretty printing metrics during training/testing."""
-    def __init__(self, width=None):
-        self.rank = RANK
+    def __init__(self, theme: dict = None):
         try:
             # pylint:disable=import-outside-toplevel
             from rich.console import Console as RichConsole
             from rich.theme import Theme
-            theme = None
-            if in_notebook():
-                theme = Theme({
-                    'repr.number': 'bold bright_green',
-                    'repr.attrib_name': 'bold bright_magenta',
-                    'repr.str': '#FFFF00',
-                })
+            if theme is None:
+                if in_notebook():
+                    theme = {
+                        'repr.number': 'bold #87ff00',
+                        'repr.attrib_name': 'bold #ff5fff',
+                        'repr.str': 'italic #FFFF00',
+                    }
+
+
             console = RichConsole(record=False, log_path=False,
                                   force_jupyter=in_notebook(),
                                   log_time_format='[%X] ',
-                                  theme=theme)
+                                  theme=Theme(theme))#, width=width)
+
         except (ImportError, ModuleNotFoundError):
             console = Console()
 
-        self.width = width
         self.console = console
 
     def rule(self, s: str, *args, **kwargs):
         """Print horizontal line."""
-        w = self.width - (8 + len(s))
-        hw = w // 2
-        rule = ' '.join((hw * '-', f'{s}', hw * '-'))
-        self.console.log(f'{rule}\n', *args, **kwargs)
+        self.console.rule(s, *args, **kwargs)
 
     def log(self, s: str, *args, **kwargs):
+        """Print `s` using `self.console` object."""
         self.console.log(s, *args, **kwargs)
 
+    def load_metrics(self, infile: str = None):
+        """Try loading metrics from infile."""
+        return joblib.load(infile)
+
     def print_metrics(
-        self,
-        metrics: dict,
-        window: int = 0,
-        pre: list = None,
-        outfile: str = None,
-        keep: list[str] = None,
-        skip: list[str] = None,
+            self,
+            metrics: dict,
+            window: int = 0,
+            outfile: str = None,
+            skip: list[str] = None,
+            keep: list[str] = None,
+            pre: Union[str, list, tuple] = None,
     ):
         """Print nicely formatted string of summary of items in `metrics`."""
-        if self.rank != 0:
-            return
+        if skip is None:
+            skip = []
 
         if keep is None:
             keep = list(metrics.keys())
 
-        if skip is None:
-            skip = []
-
-        outstr = ' '.join([
+        fstrs = [
             strformat(k, v, window) for k, v in metrics.items()
-            if k not in skip
-            and k in keep
-        ])
+            if k in keep and k not in skip and not isinstance(v, AttrDict)
+        ]
         if pre is not None:
-            outstr = ' '.join([*pre, outstr])
+            fstrs = [pre, *fstrs] if isinstance(pre, str) else [*pre] + fstrs
 
+        outstr = ' '.join(fstrs)
         self.log(outstr)
         if outfile is not None:
             with open(outfile, 'a') as f:
@@ -164,22 +169,53 @@ class Logger:
         return outstr
 
     def save_metrics(
-        self,
-        metrics: dict,
-        outfile: str = None,
-        tstamp: str = None,
+            self,
+            metrics: dict,
+            outfile: str = None,
+            tstamp: str = None,
     ):
-        """Save `metrics` to compressed `.z.` file."""
-        #  if tstamp is None:
-        #      tstamp = get_timestamp('%Y-%m-%d-%H%M%S')
-        #
-        #  if outfile is None:
-        #      outdir = os.path.join(os.getcwd(), tstamp)
-        #      fname = 'metrics.z'
-        #  else:
-        #      outdir, fname = os.path.split(outfile)
-        #
-        #  check_else_make_dir(outdir)
-        #  outfile = os.path.join(os.getcwd(), tstamp
-        # TODO: rethink this
-        pass
+        """Save metrics to compressed `.z.` file."""
+        if tstamp is None:
+            tstamp = get_timestamp('%Y-%m-%d-%H%M%S')
+
+        if outfile is None:
+            outdir = os.path.join(os.getcwd(), tstamp)
+            fname = 'metrics.z'
+
+        else:
+            outdir, fname = os.path.split(outfile)
+
+        check_else_make_dir(outdir)
+        outfile = os.path.join(os.getcwd(), tstamp, 'metrics.z')
+        self.log(f'Saving metrics to: {os.path.relpath(outdir)}')
+        savez(metrics, outfile, name=fname.split('.')[0])
+
+
+def check_else_make_dir(outdir: Union[str, Path, list, tuple]):
+    if isinstance(outdir, (str, Path)) and not os.path.isdir(str(outdir)):
+        Logger().log(f'Creating directory: {os.path.relpath(outdir)}')
+        os.makedirs(str(outdir))
+
+    elif isinstance(outdir, (tuple, list)):
+        _ = [check_else_make_dir(str(d)) for d in outdir]
+
+
+def loadz(infile: str):
+    return joblib.load(infile)
+
+
+def savez(obj: Any, fpath: str, name: str = None):
+    """Save `obj` to compressed `.z` file at `fpath`."""
+    head, _ = os.path.split(fpath)
+
+    check_else_make_dir(head)
+
+    if not fpath.endswith('.z'):
+        fpath += '.z'
+
+    if name is not None:
+        Logger().log(f'Saving {name} to {os.path.relpath(fpath)}.')
+    else:
+        Logger().log(f'Saving {obj.__class__} to {os.path.relpath(fpath)}.')
+
+    joblib.dump(obj, fpath)
