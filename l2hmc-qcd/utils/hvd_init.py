b827cff071ca7eda5e4c489e4919e9948b26d0ab
Sam Foreman <saforem2@gmail.com>
2021-08-08 17:55:19 -0500

    Wraps `utils/hvd_init.py` with rich logger

--- l2hmc-qcd/utils/hvd_init.py
+++ l2hmc-qcd/utils/hvd_init.py
@@ -5,15 +5,10 @@ Initialize Horovod and check ranks.
 """
 from __future__ import absolute_import, division, print_function, annotations
 
-import logging
 import tensorflow as tf
+from utils.logger import Logger
 
-RANK = 0
-SIZE = 1
-LOCAL_SIZE = 1
-LOCAL_RANK = 0
-IS_CHIEF = True
-HAS_HOROVOD = False
+logger = Logger()
 
 try:
     import horovod
@@ -32,12 +27,16 @@ try:
     #  logging.info(f'using horovod from: {horovod.__file__}')
     #  logging.info(f'using horovod version: {horovod.__version__}')
     prefix = f'{RANK} / {SIZE} ::'
-    logging.info(80 * '=')
-    logging.info(f'{prefix} Using tensorflow version: {tf.__version__}')
-    logging.info(f'{prefix} Using tensorflow from: {tf.__file__}')
-    logging.info(f'{prefix} Using horovod version: {horovod.__version__}')
-    logging.info(f'{prefix} Using horovod from: {horovod.__file__}')
-    logging.info(80 * '=')
+    if IS_CHIEF:
+        logger.info(80 * '=')
+        logger.info(f'{prefix} Using tensorflow version: {tf.__version__}')
+        logger.info(f'{prefix} Using tensorflow from: {tf.__file__}')
+        logger.info(f'{prefix} Using horovod version: {horovod.__version__}')
+        logger.info(f'{prefix} Using horovod from: {horovod.__file__}')
+        logger.info(80 * '=')
+    else:
+        logger.info(f'Hello, im rank: {RANK} of {SIZE} total ranks')
+
     GPUS = tf.config.experimental.list_physical_devices('GPU')
     for gpu in GPUS:
         tf.config.experimental.set_memory_growth(gpu, True)
@@ -46,5 +45,11 @@ try:
         tf.config.experimental.set_visible_devices(gpu, 'GPU')
 
 except (ImportError, ModuleNotFoundError) as err:
-    logging.warning('Unable to initialize horovod!!!')
-    logging.warning(err)
+    RANK = 0
+    SIZE = 1
+    LOCAL_SIZE = 1
+    LOCAL_RANK = 0
+    IS_CHIEF = True
+    HAS_HOROVOD = False
+    logger.error('Unable to initialize horovod!!!')
+    logger.error(err)
