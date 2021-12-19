f012d40ff71c7d3938cd39037082d9cf6ae0951c
Sam Foreman <saforem2@gmail.com>
2021-12-19 07:53:35 -0600

    Updates to network modules
    Changes:
     - `network/config.py`
     - `network/functional_net.py`
    
    Former-commit-id: cf13cd6f8d71a72784b27ac428597b8bde164382

--- l2hmc-qcd/network/config.py
+++ l2hmc-qcd/network/config.py
@@ -3,34 +3,52 @@ config.py
 
 Contains configuration objects for networks.
 """
-from dataclasses import dataclass
+from __future__ import absolute_import, division, print_function, annotations
+
+import json
+from os import PathLike
+from typing import Callable, Union
+
 import tensorflow as tf
 
+from dataclasses import asdict, dataclass, field
+
+
 @dataclass
-class LearningRateConfig:
+class BaseConfig:
+    def to_json(self) -> str:
+        return json.dumps(asdict(self))
+
+    def to_file(self, fpath: PathLike):
+        with open(fpath, 'w') as f:
+            json.dump(self.to_json(), f, indent=4)
+
+
+@dataclass
+class LearningRateConfig(BaseConfig):
     """Learning rate configuration object."""
     lr_init: float
-    decay_steps: int
-    decay_rate: float
-    warmup_steps: int = 0
+    decay_steps: int = field(default_factory=int)
+    decay_rate: float = field(default_factory=float)
+    warmup_steps: int = field(default_factory=int)
 
 
 @dataclass
-class NetworkConfig:
+class NetworkConfig(BaseConfig):
     """Network configuration object."""
     units: list
-    dropout_prob: float = 0.
-    activation_fn: callable = tf.nn.relu
-    use_batch_norm: bool = True
+    activation_fn: Callable
+    dropout_prob: float = field(default_factory=float)
+    use_batch_norm: bool = field(default_factory=bool)
 
 
 @dataclass
-class ConvolutionConfig:
+class ConvolutionConfig(BaseConfig):
     """Convolutional network configuration."""
     input_shape: tuple
     filters: tuple
     sizes: tuple
-    pool_sizes: tuple
-    conv_activations: tuple = None
-    conv_paddings: tuple = None
-    use_batch_norm: bool = True
+    pool: tuple
+    activation: Callable = field(default_factory=Callable)
+    paddings: tuple = field(default_factory=tuple)
+    use_batch_norm: bool = field(default_factory=bool)
