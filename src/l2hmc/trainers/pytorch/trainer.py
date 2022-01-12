1d35c43a0e850fd056072e7ae24685defc827485
Sam Foreman <saforem2@gmail.com>
2022-01-12 07:24:57 -0600

    Updates to `src/l2hmc/trainers/`
    Updated methods for training L2HMC dynamics
    
    
    Former-commit-id: 02fb1a5e91cd6ed8f052dfe32549b6207ae9b70f

--- src/l2hmc/trainers/pytorch/trainer.py
+++ src/l2hmc/trainers/pytorch/trainer.py
@@ -8,79 +8,104 @@ from __future__ import absolute_import, division, print_function, annotations
 import torch
 import time
 from torch import optim
+from rich.console import Console
+from typing import Callable
 import numpy as np
 from src.l2hmc.configs import Steps
 from src.l2hmc.dynamics.pytorch.dynamics import Dynamics, to_u1, random_angle
 from src.l2hmc.loss.pytorch.loss import LatticeLoss
+from src.l2hmc.utils.history import History, StateHistory
 
 Tensor = torch.Tensor
+
+console = Console(color_system='truecolor', log_path=False, )
 
 
 def train_step(
         inputs: tuple[Tensor, float],
         dynamics: Dynamics,
         optimizer: optim.Optimizer,
-        loss_fn: LatticeLoss,
+        loss_fn: Callable = LatticeLoss,
 ) -> tuple[Tensor, dict]:
     start = time.time()
     dynamics.train()
     x_init, beta = inputs
-
     if torch.cuda.is_available():
         x_init = x_init.cuda()
 
-    x_out, metrics = dynamics((to_u1(x_init), beta))
-    x_prop = metrics.pop('mc_states').proposed.x
-    loss = loss_fn(x_init=x_init, x_prop=x_prop, acc=metrics['acc'])
+    x_out, tmetrics = dynamics((to_u1(x_init), beta))
+    x_prop = tmetrics.get('mc_states').proposed.x
+    loss = loss_fn(x_init=x_init, x_prop=x_prop, acc=tmetrics['acc'])
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    metrics['dt'] = time.time() - start
-    metrics['loss'] = loss.detach().cpu().numpy()
+    metrics = {
+        'dt': time.time() - start,
+        'loss': loss.detach().cpu().numpy(),
+    }
+    for key, val in tmetrics.items():
+        if isinstance(val, Tensor):
+            metrics[key] = val.detach().cpu().numpy()
+        else:
+            metrics[key] = val
 
     return to_u1(x_out).detach(), metrics
 
 
 def train(
-        dynamics: Dynamics,
-        optimizer: optim.Optimizer,
-        loss_fn: LatticeLoss,
-        steps: Steps,
-        beta: list[float] | float,
-        x: Tensor = None,
-        window: int = 10,
-        skip: list[str] | str = None,
-        keep: list[str] | str = None,
-        history: dict = None,
+    steps: Steps,
+    dynamics: Dynamics,
+    beta: float,
+    optimizer: optim.Optimizer,
+    loss_fn: Callable = LatticeLoss,
+    xinit: Tensor = None,
+    record_states: bool = False,
 ) -> dict:
-    dynamics.train()
+    """Train the L2HMC sampler."""
+    history = History(steps)
+    states = StateHistory()
+    mstrs = []
 
+    x = xinit
     if x is None:
         x = random_angle(dynamics.xshape, requires_grad=True)
         x = x.reshape(x.shape[0], -1)
 
-    train_logs = []
-    if history is None:
-        history = {}
+    for era in range(steps.nera):
+        console.rule(f'ERA: {era}')
+        estart = time.time()
+        for epoch in range(steps.nepoch):
+            x, metrics = train_step((x, beta),
+                                    dynamics=dynamics,
+                                    optimizer=optimizer,
+                                    loss_fn=loss_fn)
+            if (epoch % steps.log == 0) or (epoch % steps.print == 0):
+                record = {
+                    'era': era,
+                    'epoch': epoch,
+                }
+                # NOTE:
+                # Pop (large) mc_states from tmetrics before updating metrics
+                # preventing them from being tracked unless explicitly asked
+                mc_states = metrics.pop('mc_states', None)
+                if record_states and mc_states is not None:
+                    states.update(mc_states)
 
-    if isinstance(beta, list):
-        assert len(beta) == steps.train
+                # Update metrics with train step metrics, tmetrics
+                record.update(metrics)
+                mstr = history.update(record)
+                mstrs.append(mstr)
+                if epoch % steps.print == 0:
+                    console.log(mstr)
 
-    elif isinstance(beta, float):
-        beta = np.array(steps.train * [beta], dtype=np.float32).tolist()
-    else:
-        raise TypeError(f'beta expected to be `float | list[float]`,\n got: {type(beta)}')
+        console.rule()
+        console.log(f'Era {era} took: {time.time() - estart:<3.2g}s')
+        console.log(f'Avgs over last era:\n {history.era_summary(era)}')
+        console.rule()
 
-
-    assert (isinstance(beta, list) and isinstance(beta[0], float))
-    for step, b in zip(range(steps.train), beta):
-        x, metrics = train_step((to_u1(x), b),
-                                dynamics=dynamics,
-                                optimizer=optimizer,
-                                loss_fn=loss_fn)
-        if (step + 1) % steps.log == 0:
-            for key, val in metrics.items():
-                try:
-                    history[key].append(val)
-                except KeyError:
-                    history[key] = [val]
+    return {
+        'history': history,
+        'dynamics': dynamics,
+        'optimizer': optimizer,
+        'state_history': states,
+    }
--- src/l2hmc/trainers/tensorflow/trainer.py
+++ src/l2hmc/trainers/tensorflow/trainer.py
@@ -3,36 +3,123 @@ trainer.py
 
 Implements methods for training L2HMC sampler
 """
-from __future__ import division, absolute_import, print_function, annotations
-
-import tensorflow as tf
+from __future__ import absolute_import, annotations, division, print_function
 import time
 from typing import Callable
-from src.l2hmc.dynamics.tensorflow.dynamics import Dynamics, to_u1
 
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.optimizers import Optimizer
+from rich.console import Console
+
+from src.l2hmc.configs import Steps
+from src.l2hmc.dynamics.tensorflow.dynamics import Dynamics, to_u1
 from src.l2hmc.loss.tensorflow.loss import LatticeLoss
 
-from tensorflow.keras.optimizers import Optimizer
+from src.l2hmc.utils.history import StateHistory, History
 
 Tensor = tf.Tensor
+console = Console(color_system='truecolor', log_path=False)
 
+
+class Trainer:
+    def __init__(
+        self,
+        dynamics: Dynamics,
+        beta: list,
+        optimizer: Optimizer,
+        loss_fn: Callable = LatticeLoss,
+    ):
+        pass
+
+
+@tf.function
 def train_step(
-        inputs: tuple[Tensor, Tensor],
+        inputs: tuple[Tensor, Tensor | float],
         dynamics: Dynamics,
         optimizer: Optimizer,
         loss_fn: Callable = LatticeLoss,
 ) -> tuple[Tensor, dict]:
-    start = time.time()
     x_init, beta = inputs
-
     with tf.GradientTape() as tape:
-        x_out, metrics = dynamics((to_u1(x_init), beta))
-        x_prop = metrics.pop('mc_states').proposed.x
-        loss = loss_fn(x_init=x_init, x_prop=x_prop, acc=metrics['acc'])
+        x_out, tmetrics = dynamics((to_u1(x_init), tf.constant(beta)))
+        x_prop = tmetrics.pop('mc_states').proposed.x
+        # x_prop = metrics.pop('mc_states').proposed.x
+        loss = loss_fn(x_init=x_init, x_prop=x_prop, acc=tmetrics['acc'])
 
     grads = tape.gradient(loss, dynamics.trainable_variables)
     optimizer.apply_gradients(zip(grads, dynamics.trainable_variables))
-    metrics['dt'] = time.time() - start
-    metrics['loss'] = loss
+    metrics = {
+        'loss': loss,
+    }
+    for key, val in tmetrics.items():
+        metrics[key] = val
 
     return to_u1(x_out), metrics
+
+
+def setup_beta(beta: list | float):  # -> list[float]
+    # TODO: Deal with making sure len(beta) == len(train_steps)
+    beta = beta
+    pass
+
+
+def train(
+        steps: Steps,
+        dynamics: Dynamics,
+        beta: float,
+        optimizer: Optimizer,
+        loss_fn: Callable = LatticeLoss,
+        xinit: Tensor = None,
+        record_states: bool = False,
+) -> dict:
+    """Train the L2HMC sampler"""
+    history = History(steps)
+    states = StateHistory()
+    mstrs = []
+
+    if xinit is None:
+        x = tf.random.uniform(dynamics.xshape, *(-np.pi, np.pi))
+        x = tf.reshape(x, (x.shape[0], -1))
+    else:
+        x = tf.constant(xinit)
+
+    assert isinstance(x, Tensor)
+    dynamics.compile(optimizer=optimizer, loss=loss_fn)
+    # train_step_ = tf.function(train_step)
+    for era in range(steps.nera):
+        console.rule(f'ERA: {era}')
+        estart = time.time()
+        for epoch in range(steps.nepoch):
+            tstart = time.time()
+            x, metrics = train_step((x, beta),
+                                    dynamics=dynamics,
+                                    optimizer=optimizer,
+                                    loss_fn=loss_fn)
+            if (epoch % steps.log == 0) or (epoch % steps.print == 0):
+                record = {
+                    'era': era,
+                    'epoch': epoch,
+                    'dt': time.time() - tstart,
+                }
+                mc_states = metrics.pop('mc_states', None)
+                if record_states and mc_states is not None:
+                    states.update(mc_states)
+
+                record.update(metrics)
+                mstr = history.update(record)
+                mstrs.append(mstr)
+                if epoch % steps.print == 0:
+                    console.log(mstr)
+
+        console.rule()
+        console.log(f'Era {era} took: {time.time() - estart:<3.2g}s')
+        console.log(f'Avgs over last era:\n {history.era_summary(era)}')
+        console.rule()
+
+    return {
+        'history': history,
+        'dynamics': dynamics,
+        'optimizer': optimizer,
+        'state_history': states,
+    }
