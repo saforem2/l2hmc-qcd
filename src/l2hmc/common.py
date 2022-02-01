5084b35786cc61bddd31184e9245a518b8d50bd7
Sam Foreman <saforem2@gmail.com>
2022-02-01 10:53:42 -0600

    Merge `train_tensorflow.py` `train_pytorch.py` into `main.py`
    Specify framework by passing argument:
    
    ```bash
    $ python3 main.py framework=pytorch|tensorflow
    ```

--- src/l2hmc/common.py
+++ src/l2hmc/common.py
@@ -4,15 +4,19 @@ l2hmc/common.py
 Contains methods intended to be shared across frameworks.
 """
 from __future__ import absolute_import, annotations, division, print_function
-import os
-
-from omegaconf import DictConfig
-from pathlib import Path
-import xarray as xr
-from l2hmc.utils.plot_helpers import plot_dataArray
-from l2hmc.utils.console import console
-from rich.table import Table
 import datetime
+import logging
+import os
+from pathlib import Path
+from typing import Any
+
+import joblib
+import matplotlib.pyplot as plt
+import matplotx
+import numpy as np
+from omegaconf import DictConfig
+from rich.table import Table
+import xarray as xr
 
 from l2hmc.configs import (
     DynamicsConfig,
@@ -22,6 +26,10 @@ from l2hmc.configs import (
     NetworkConfig,
     Steps,
 )
+from l2hmc.utils.console import console, is_interactive
+from l2hmc.utils.plot_helpers import plot_dataArray
+
+log = logging.getLogger(__name__)
 
 
 def get_timestamp(fstr=None):
@@ -51,8 +59,8 @@ def setup_pytorch(configs: dict) -> dict:
     xshape = dynamics_config.xshape
 
     input_spec = InputSpec(xshape=xshape,
-                           vnet={'v': (xdim,), 'x': (xdim,)},
-                           xnet={'v': (xdim,), 'x': (xdim, 2)})
+                           vnet={'v': [xdim, ], 'x': [xdim, ]},
+                           xnet={'v': [xdim, ], 'x': [xdim, 2]})
     network_factory = NetworkFactory(input_spec=input_spec,
                                      net_weights=net_weights,
                                      network_config=network_config)
@@ -83,12 +91,12 @@ def setup_pytorch(configs: dict) -> dict:
 
 
 def setup_tensorflow(configs: dict) -> dict:
-    import tensorflow as tf
     from l2hmc.dynamics.tensorflow.dynamics import Dynamics
     from l2hmc.lattice.tensorflow.lattice import Lattice
     from l2hmc.loss.tensorflow.loss import LatticeLoss
     from l2hmc.network.tensorflow.network import NetworkFactory
     from l2hmc.trainers.tensorflow.trainer import Trainer
+    import tensorflow as tf
 
     steps = configs['steps']
     loss_config = configs['loss_config']
@@ -100,8 +108,8 @@ def setup_tensorflow(configs: dict) -> dict:
     xshape = dynamics_config.xshape
 
     input_spec = InputSpec(xshape=xshape,
-                           vnet={'v': (xdim,), 'x': (xdim,)},
-                           xnet={'v': (xdim,), 'x': (xdim, 2)})
+                           vnet={'v': [xdim, ], 'x': [xdim, ]},
+                           xnet={'v': [xdim, ], 'x': [xdim, 2]})
     network_factory = NetworkFactory(input_spec=input_spec,
                                      net_weights=net_weights,
                                      network_config=network_config)
@@ -141,10 +149,41 @@ def setup_common(cfg: DictConfig) -> dict:
     }
 
 
+def plot_plaqs(
+        plaqs: np.ndarray,
+        nchains: int = 10,
+        outdir: os.PathLike = None,
+):
+    # plaqs = []
+    # for x in xarr:
+    #     plaqs.append(lattice.plaqs_diff(beta, x=x))
+
+    # plaqs = np.array(plaqs)
+    assert len(plaqs.shape) == 2
+    ndraws, nchains = plaqs.shape
+    xplot = np.arange(ndraws)
+    fig, ax = plt.subplots(constrained_layout=True)
+    _ = ax.plot(xplot, plaqs.mean(-1), label='avg', lw=2.0, color='C0')
+    for idx in range(min(nchains, plaqs.shape[1])):
+        _ = ax.plot(xplot, plaqs[:, idx], lw=1.0, alpha=0.5, color='C0')
+
+    _ = ax.set_ylabel(r'$\delta x_{P}$')
+    _ = ax.set_xlabel('Train Epoch')
+    _ = ax.grid(True, alpha=0.4)
+    _ = matplotx.line_labels()
+    if outdir is not None:
+        outfile = Path(outdir).joinpath('plaqs_diffs.svg')
+        log.info(f'Saving figure to: {outfile}')
+        fig.savefig(outfile.as_posix(), dpi=500, bbox_inches='tight')
+
+    return fig, ax
+
+
 def plot_dataset(
     dataset: xr.Dataset,
-    nchains: int = 0,
+    nchains: int = 10,
     outdir: os.PathLike = None,
+    title: str = None,
 ) -> None:
     if outdir is None:
         outdir = Path(os.getcwd()).joinpath('plots', 'training')
@@ -152,13 +191,16 @@ def plot_dataset(
         outdir = Path(outdir)
 
     for key, val in dataset.data_vars.items():
+        if key == 'x':
+            continue
+
         fig, _, _ = plot_dataArray(val,
                                    key=key,
-                                   title='PyTorch',
+                                   title=title,
                                    num_chains=nchains)
         outfile = outdir.joinpath(f'{key}.svg')
         outfile.parent.mkdir(exist_ok=True, parents=True)
-        console.log(f'Saving figure to: {outfile.as_posix()}')
+        log.info(f'Saving figure to: {outfile.as_posix()}')
         fig.savefig(outfile.as_posix(), dpi=500, bbox_inches='tight')
 
 
@@ -171,9 +213,6 @@ def save_logs(
     else:
         logdir = Path(logdir)
 
-    for era, table in tables.items():
-        console.print(table)
-
     cfile = logdir.joinpath('console.txt').as_posix()
     text = console.export_text()
     with open(cfile, 'w') as f:
@@ -182,14 +221,104 @@ def save_logs(
     table_dir = logdir.joinpath('tables')
     tdir = table_dir.joinpath('txt')
     hdir = table_dir.joinpath('html')
-    for era, table in tables.items():
+
+    hfile = hdir.joinpath('training_table.html')
+    hfile.parent.mkdir(exist_ok=True, parents=True)
+
+    tfile = tdir.joinpath('training_table.txt')
+    tfile.parent.mkdir(exist_ok=True, parents=True)
+
+    for _, table in tables.items():
         console.print(table)
         html = console.export_html(clear=False)
-        hfile = hdir.joinpath(f'era{era}.html')
-        hfile.parent.mkdir(exist_ok=True, parents=True)
-        with open(hfile.as_posix(), 'w') as f:
+        # hfile = hdir.joinpath(f'era{era}.html')
+        with open(hfile.as_posix(), 'a') as f:
             f.write(html)
 
-        tfile = tdir.joinpath(f'era{era}.txt')
-        tfile.parent.mkdir(exist_ok=True, parents=True)
-        console.save_text(tfile.as_posix())
+        # tfile = tdir.joinpath(f'era{era}.txt')
+        text = console.export_text()
+        with open(tfile, 'a') as f:
+            f.write(text)
+
+
+def train(cfg: DictConfig) -> dict:
+    beta = cfg.get('beta', None)
+    if beta is None:
+        log.warn('Beta not specified! Using beta = 1.0')
+        beta = 1.0
+
+    save_x = cfg.get('save_x', False)
+    framework = cfg.get('framework', None)
+    width = cfg.get('width', 0)
+    assert framework is not None
+
+    kwargs = {'beta': beta, 'save_x': save_x, }
+    if width > 0:
+        kwargs['width'] = width
+
+    common = setup_common(cfg)
+    if framework in ['pytorch', 'torch', 'pt']:
+        framework = 'pytorch'
+        setup = setup_pytorch(common)
+    else:
+        if framework in ['tensorflow', 'tf']:
+            framework = 'tensorflow'
+            setup = setup_tensorflow(common)
+            kwargs.update({
+                'compile': cfg.get('compile', True),
+                'jit_compile': cfg.get('jit_compile', False),
+            })
+        else:
+            raise ValueError(f'Unexpected framework: {framework}')
+
+    log.info(f'Using {framework}, with trainer: {setup["trainer"]}')
+    output = setup['trainer'].train(**kwargs)
+    history = output['history']
+
+    dataset = history.get_dataset()
+
+    nchains = min((cfg.dynamics.xshape[0], cfg.dynamics.nleapfrog))
+
+    outdir = Path(os.getcwd())
+    outdir.mkdir(exist_ok=True, parents=True)
+
+    dirs = {'outdir': outdir}
+    for key in ['logs', 'plots', 'data']:
+        d = dirs['outdir'].joinpath(key)
+        d.mkdir(exist_ok=True, parents=True)
+        dirs[key] = d
+
+    plot_dataset(dataset,
+                 nchains=nchains,
+                 title=framework,
+                 outdir=dirs['plots'])
+    datafile = dirs['data'].joinpath('history_dataset.nc')
+    mode = 'a' if datafile.is_file() else 'w'
+    log.info(f'Saving dataset to: {datafile.as_posix()}')
+    datafile.parent.mkdir(exist_ok=True, parents=True)
+    dataset.to_netcdf(datafile.as_posix(), mode=mode)
+
+    output.update(**setup)
+    if cfg.get('save_x', False):
+        xarr = output['xarr']
+        beta = cfg.get('beta', None)
+        lattice = output['lattice']
+        plaqs_diffs = []
+        for x in xarr:
+            plaqs_diffs.append(lattice.plaqs_diff(beta=beta, x=x).numpy())
+
+        plaqs_diffs = np.array(plaqs_diffs)
+        _ = plot_plaqs(plaqs=plaqs_diffs, nchains=10, outdir=dirs['plots'])
+        xfile = dirs['data'].joinpath('xarr.z')
+        log.info(f'saving xarr to: {xfile}')
+        joblib.dump(xarr, xfile)
+
+    if is_interactive():
+        return output
+
+    log.info(
+        f'Saving logs and training table to: {dirs["logs"].as_posix()}'
+    )
+    save_logs(output['tables'], logdir=dirs['logs'])
+
+    return output
--- src/l2hmc/main.py
+++ src/l2hmc/main.py
@@ -1,10 +1,36 @@
+"""
+main.py
+
+Contains entry point for training Dynamics.
+"""
+from __future__ import absolute_import, print_function, division, annotations
 from omegaconf import DictConfig, OmegaConf
+import os
 import hydra
+import logging
+from l2hmc.common import train
+
+log = logging.getLogger(__name__)
+
+
+def train_tensorflow(cfg: DictConfig) -> dict:
+    from l2hmc.train_tensorflow import train
+    output = train(cfg)
+
+    return output
+
+
+def train_pytorch(cfg: DictConfig) -> dict:
+    from l2hmc.train_pytorch import train
+    output = train(cfg)
+    return output
 
 
 @hydra.main(config_path='./conf', config_name='config')
 def main(cfg: DictConfig) -> None:
+    print("Working directory : {}".format(os.getcwd()))
     print(OmegaConf.to_yaml(cfg))
+    _ = train(cfg)
 
 
 if __name__ == '__main__':
