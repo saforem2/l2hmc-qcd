6e19d7620fb4e5ef99a8dab119e02438310b6615
Sam Foreman <saforem2@gmail.com>
2022-03-01 02:56:18 +0000

    Updates `main_*.py`
    Former-commit-id: 27ad1c71ec53b12bd48d99cb764cc710cbc360f6

--- src/l2hmc/main_tensorflow.py
+++ src/l2hmc/main_tensorflow.py
@@ -8,9 +8,9 @@ import logging
 import os
 from pathlib import Path
 from typing import Any, Optional
-import rich
 
 import tensorflow as tf
+import numpy as np
 import horovod.tensorflow as hvd
 
 import wandb
@@ -45,6 +45,7 @@ def setup(cfg: DictConfig) -> dict:
     net_weights = instantiate(cfg.net_weights)
     schedule = instantiate(cfg.annealing_schedule)
     schedule.setup(steps)
+    clipnorm = cfg.get('clipnorm', 10.0)
 
     try:
         conv_cfg = instantiate(cfg.get('conv', None))
@@ -65,7 +66,8 @@ def setup(cfg: DictConfig) -> dict:
                         potential_fn=lattice.action,
                         network_factory=net_factory)
     loss_fn = LatticeLoss(lattice=lattice, loss_config=loss_cfg)
-    optimizer = tf.keras.optimizers.Adam(cfg.learning_rate.lr_init)
+    optimizer = tf.keras.optimizers.Adam(cfg.learning_rate.lr_init,
+                                         clipnorm=clipnorm)
     trainer = Trainer(steps=steps,
                       rank=RANK,
                       loss_fn=loss_fn,
@@ -89,9 +91,9 @@ def update_wandb_config(
         cfg: DictConfig,
         id: Optional[str] = None,
         debug: Optional[bool] = None,
-        # job_type: Optional[str] = None,
         wbconfig: Optional[dict | list | str] = None,
-        wbdir: Optional[os.PathLike] = None,
+        # job_type: Optional[str] = None,
+        # wbdir: Optional[os.PathLike] = None,
 ) -> None:
     """Updates config using runtime information for W&B."""
     group = ['tensorflow', 'horovod' if SIZE > 1 else 'local']
@@ -105,8 +107,8 @@ def update_wandb_config(
     # if job_type is not None:
     #     cfg.wandb.setup.update({'job_type': job_type})
 
-    if wbdir is not None:
-        cfg.wandb.setup.update({'dir': Path(wbdir).as_posix()})
+    # if wbdir is not None:
+    #     cfg.wandb.setup.update({'dir': Path(wbdir).as_posix()})
 
     if wbconfig is not None:
         cfg.wandb.setup.update({'config': wbconfig})
@@ -126,7 +128,7 @@ def get_summary_writer(
     writer = None
     if trainer.rank == 0:
         writer = tf.summary.create_file_writer(sdir.as_posix())  # type: ignore
-        writer.set_as_default()
+        # writer.set_as_default()
 
     return {'dir': jobdir, 'writer': writer}
 
@@ -136,32 +138,35 @@ def eval(
         trainer: Trainer,
         job_type: str,
         run: Optional[Any] = None,
+        writer: Optional[Any] = None,
+        jobdir: Optional[os.PathLike] = None,
 ) -> dict:
-    therm_frac = cfg.get('therm_frac', 0.2)
     nchains = cfg.get('nchains', -1)
-    # job_type = cfg.wandb.setup.get('job_type', 'eval')
-    # objs = _setup(cfg, trainer, job_type=job_type)
-    objs = get_summary_writer(cfg, trainer, job_type=job_type)
+    therm_frac = cfg.get('therm_frac', 0.2)
+    if jobdir is None:
+        jobdir = Path(cfg.get('outdir', os.getcwd())).joinpath(job_type)
 
+    assert jobdir is not None
+    jobdir = Path(jobdir)
     eval_output = trainer.eval(run=run,
-                               writer=objs['writer'],
-                               hmc=(job_type == 'hmc'),
+                               writer=writer,
+                               job_type=job_type,
                                width=cfg.get('width', None))
     eval_dset = eval_output['history'].get_dataset(therm_frac=therm_frac)
     _ = analyze_dataset(eval_dset,
+                        outdir=jobdir,
                         nchains=nchains,
-                        outdir=objs['dir'],
                         title=f'{job_type}: PyTorch')
     if not is_interactive():
-        edir = objs['dir'].joinpath('logs')
+        edir = jobdir.joinpath('logs')
         edir.mkdir(exist_ok=True, parents=True)
         log.info(f'Saving {job_type} logs to: {edir.as_posix()}')
         save_logs(logdir=edir,
                   tables=eval_output['tables'],
                   summaries=eval_output['summaries'])
 
-    if objs['writer'] is not None:
-        objs['writer'].close()
+    if writer is not None:
+        writer.close()
 
     return eval_output
 
@@ -170,37 +175,45 @@ def train(
         cfg: DictConfig,
         trainer: Trainer,
         run: Optional[Any] = None,
+        writer: Optional[Any] = None,
+        jobdir: Optional[os.PathLike] = None,
         **kwargs,
 ) -> dict:
-    objs = get_summary_writer(cfg, trainer, job_type='train')
+    if jobdir is None:
+        jobdir = Path(cfg.get('outdir', os.getcwd())).joinpath('train')
+
+    jobdir = Path(jobdir)
+    jobdir.mkdir(exist_ok=True, parents=True)
+    # objs = get_summary_writer(cfg, trainer, job_type='train')
     # objs = _setup(cfg, trainer, job_type='train')
     train_output = trainer.train(run=run,
-                                 writer=objs['writer'],
-                                 train_dir=objs['dir'],
+                                 writer=writer,
+                                 train_dir=jobdir,
                                  width=cfg.get('width', None),
                                  **kwargs)
     if trainer.rank == 0:
         dset = train_output['history'].get_dataset()
         _ = analyze_dataset(dset,
-                            outdir=objs['dir'],
+                            outdir=jobdir,
                             prefix='train',
                             title='Training: TensorFlow',
                             nchains=cfg.get('nchains', -1))
         if not is_interactive():
-            tdir = objs['dir'].joinpath('logs')
+            tdir = jobdir.joinpath('logs')
             tdir.mkdir(exist_ok=True, parents=True)
             log.info(f'Saving train logs to: {tdir.as_posix()}')
             save_logs(logdir=tdir,
                       tables=train_output['tables'],
                       summaries=train_output['summaries'])
 
-    if objs['writer'] is not None:
-        objs['writer'].close()
+    if writer is not None:
+        writer.close()
 
     return train_output
 
 
 def main(cfg: DictConfig) -> dict:
+    outputs = {}
     objs = setup(cfg)
     trainer = objs['trainer']  # type: Trainer
     nchains = min((cfg.dynamics.xshape[0], cfg.dynamics.nleapfrog))
@@ -219,38 +232,37 @@ def main(cfg: DictConfig) -> dict:
     if trainer.rank == 0:
         run = wandb.init(**cfg.wandb.setup)
 
+    writers = {
+        'train': get_summary_writer(cfg, trainer, job_type='train'),
+        'eval': get_summary_writer(cfg, trainer, job_type='eval'),
+        'hmc': get_summary_writer(cfg, trainer, job_type='hmc'),
+    }
+
     kwargs = {
         'compile': cfg.get('compile', True),
         'jit_compile': cfg.get('jit_compile', False),
     }
-    train_output = train(cfg, trainer, run=run, **kwargs)
-    hmc_output = None
-    eval_output = None
+    outputs['train'] = train(cfg, trainer, run=run, **kwargs)
 
     if trainer.rank == 0:
-        cfg.wandb.setup.update({
-            # 'job_type': 'eval',
-            'tags': [f'beta={cfg.annealing_schedule.beta_final:1.2f}'],
-        })
-        log.warning('Evaluating trained model')
-        eval_output = eval(cfg=cfg, trainer=trainer, job_type='eval', run=run)
+        for job in ['eval', 'hmc']:
+            log.warning(f'Running {job}')
+            outputs[job] = eval(cfg=cfg,
+                                run=run,
+                                job_type=job,
+                                trainer=trainer,
+                                writer=writers[job]['writer'])
+        # cfg.wandb.setup.update({
+        #     # 'job_type': 'eval',
+        #     'tags': [f'beta={cfg.annealing_schedule.beta_final:1.2f}'],
+        # })
 
-        # cfg.wandb.setup.update({'job_type': 'hmc'})
-        log.warning('Running generic HMC for comparison')
-        hmc_output = eval(cfg=cfg, trainer=trainer, job_type='hmc', run=run)
-
-    return {
-        'train': train_output,
-        'eval': eval_output,
-        'hmc': hmc_output,
-    }
+    return outputs
 
 
 @hydra.main(config_path='./conf', config_name='config')
 def launch(cfg: DictConfig) -> None:
-    log.info(f'Working directory: {os.getcwd()}')
-    # rich.print(OmegaConf.to_container(cfg, resolve=True))
-    # log.info(OmegaConf.to_yaml(cfg))
+    # log.info(f'Working directory: {os.getcwd()}')
     _ = main(cfg)
 
 
