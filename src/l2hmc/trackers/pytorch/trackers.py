70a8a527657038c178197d39b7c7b42095aef14a
Sam Foreman <saforem2@gmail.com>
2022-04-10 08:58:52 -0500

    Updates `trackers/*`
    Former-commit-id: 6f12c9ed3ad0daecd348fe8f8a065c34a7e05bf3

--- src/l2hmc/trackers/pytorch/trackers.py
+++ src/l2hmc/trackers/pytorch/trackers.py
@@ -5,11 +5,12 @@ Contains various utilities for tracking / logging metrics in TensorBoard
 """
 from __future__ import absolute_import, annotations, division, print_function
 import logging
-from typing import Optional, Union
+from typing import Optional, Union, Any
 
 import numpy as np
+import wandb
 import torch
-from torch.utils.tensorboard.writer import SummaryWriter
+# from torch.utils.tensorboard.writer import SummaryWriter
 
 Tensor = torch.Tensor
 Array = np.ndarray
@@ -22,77 +23,108 @@ log = logging.getLogger(__name__)
 def log_item(
         tag: str,
         val: ArrayLike,
-        writer: SummaryWriter,
-        step: Optional[int],
+        run: Optional[Any] = None,
+        step: Optional[Any] = None,
+        commit: Optional[bool] = True,
 ):
     if isinstance(val, Tensor):
         val = val.detach().cpu().numpy()
 
-    if (
-            'dt' in tag
-            or 'beta' in tag
-            or 'era' in tag
-            or 'epoch' in tag
-            or isinstance(val, (float, int, bool))
-    ):
-        writer.add_scalar(tag=tag, scalar_value=val, global_step=step)
-    else:
-        writer.add_histogram(tag=tag, values=val, global_step=step)
-        writer.add_scalar(tag=f'{tag}/avg',
-                          global_step=step,
-                          scalar_value=val.mean())
+    iter_tag = '/'.join([tag.split('/')[0]] + ['iter'])
+    data = {tag: val} if step is None else {tag: val, iter_tag: step}
+
+    if isinstance(val, np.ndarray):
+        data.update({f'{tag}/avg': val.mean()})
+        if len(val.shape) > 1:
+            data.update({
+                tag: val.reshape(val.shape[0], -1),
+            })
+
+    if run is not None:
+        run.log(data, commit=commit)
 
 
 def log_dict(
-        writer: SummaryWriter,
         d: dict,
+        run: Optional[Any] = None,
         step: Optional[int] = None,
-        prefix: Optional[str] = None
+        prefix: Optional[str] = None,
+        commit: Optional[bool] = True,
 ):
-    """Create TensorBoard summaries for all items in `d`."""
-    for key, val in d.items():
+    """Create summaries for all items in `d`."""
+    for idx, (key, val) in enumerate(d.items()):
+        c = (commit and idx == len(list(d.keys())) - 1)
+        pre = key if prefix is None else f'{prefix}/{key}'
         if isinstance(val, dict):
-            log_dict(writer=writer, d=val, step=step, prefix=f'{prefix}/{key}')
+            log_dict(run=run, step=step,
+                     d=val, prefix=pre, commit=(c and commit))
         else:
-            log_item(writer=writer, val=val, step=step, tag=f'{prefix}/{key}')
+            log_item(run=run, step=step, val=val, tag=pre, commit=c)
 
 
 def log_list(
-        writer: SummaryWriter,
         x: list,
         prefix: str,
+        run: Optional[Any] = None,
+        commit: Optional[bool] = True,
         step: Optional[int] = None,
 ):
-    """Create TensorBoard summaries for all entries in `x`."""
-    for t in x:
+    """Create summaries for all entries in `x`."""
+    for idx, t in enumerate(x):
         if isinstance(t, Tensor):
             t = t.detach().numpy()
 
         name = getattr(t, 'name', None)
         tag = f'{prefix}/{name}' if name is not None else prefix
-        log_item(writer=writer, val=t, step=step, tag=tag)
+        c = (commit and idx == len(x) - 1)
+        log_item(run=run, val=t, step=step, tag=tag, commit=c)
 
 
 def update_summaries(
-        writer: SummaryWriter,
+        run: Optional[Any] = None,
         step: Optional[int] = None,
         metrics: Optional[dict[str, ArrayLike]] = None,
         model: Optional[torch.nn.Module] = None,
         optimizer: Optional[torch.optim.Optimizer] = None,
         prefix: Optional[str] = None,
 ) -> None:
+    commit = (model is None and optimizer is None)
+    if run is not None and metrics is not None:
+        if prefix is not None:
+            run.log({prefix: metrics}, commit=commit)
+        else:
+            run.log(metrics, commit=commit)
+
     if metrics is not None:
-        log_dict(writer=writer, d=metrics, step=step, prefix=prefix)
+        log_dict(d=metrics, step=step, prefix=prefix, run=run)
+        if run is not None and run is wandb.run:
+            if prefix is not None:
+                run.log({prefix: metrics}, commit=commit)
+            else:
+                run.log(metrics, commit=commit)
     assert isinstance(step, int) if step is not None else None
     if model is not None:
+        commit = (optimizer is None)
         for name, param in model.named_parameters():
             if param.requires_grad:
                 tag = f'model/{name}'
-                log_item(writer=writer, val=param.detach(), step=step, tag=tag)
+                val = param.detach()
+                log_item(run=run, val=val, step=step, tag=tag, commit=commit)
 
     if optimizer is not None:
-        for group in optimizer.param_groups:
-            for p in group['params']:
-                if p.grad is not None:
-                    tag = f'optimizer/{getattr(p, "name", str(p))}'
-                    log_item(writer=writer, val=p.detach(), step=step, tag=tag)
+        assert isinstance(optimizer, torch.optim.Optimizer)
+        state_dict = optimizer.state_dict()
+        param_groups = state_dict.get('param_groups', None)
+        if param_groups is not None:
+            for idx, (key, val) in enumerate(param_groups.items()):
+                c = (idx == (len(list(param_groups.keys())) - 1))
+                if isinstance(val, dict):
+                    log_dict(d=val,
+                             step=step,
+                             run=run,
+                             commit=c,
+                             prefix=f'optimizer/{key}')
+                elif isinstance(val, Tensor) and val.grad is not None:
+                    t = f'optimizer/{key}'
+                    log_item(run=run, val=val.detach(), step=step, tag=t,
+                             commit=c)
--- src/l2hmc/trackers/tensorflow/trackers.py
+++ src/l2hmc/trackers/tensorflow/trackers.py
@@ -5,24 +5,48 @@ Contains various utilities for tracking / logging metrics in TensorBoard
 """
 from __future__ import absolute_import, annotations, division, print_function
 import logging
-from typing import Optional, Union
+from typing import Optional, Union, Any
 
 import numpy as np
 import tensorflow as tf
+import wandb
 from tensorflow.python.keras import backend as K
 
 
 log = logging.getLogger(__name__)
 
-Model = tf.keras.models.Model
-Optimizer = tf.keras.optimizers.Optimizer
-Tensor = tf.Tensor
 Array = np.ndarray
+Tensor = tf.Tensor
+Model = tf.keras.models.Model
 Scalar = Union[float, int, bool]
 ArrayLike = Union[Tensor, Array, Scalar]
+Optimizer = tf.keras.optimizers.Optimizer
 
 
 def log_item(
+        tag: str,
+        val: ArrayLike,
+        run: Optional[Any] = None,
+        step: Optional[int] = None,
+        commit: Optional[bool] = True,
+):
+    iter_tag = '/'.join([tag.split('/')[0]] + ['iter'])
+    tag = tag.rstrip(':0')
+    data = {tag: val} if step is None else {tag: val, iter_tag: step}
+    if isinstance(val, Tensor):
+        assert tf.is_tensor(val)
+        val = val.numpy()  # type:ignore
+
+    if isinstance(val, np.ndarray):
+        data.update({f'{tag}/avg': val.mean()})
+        if len(val.shape) > 1:
+            data.update({tag: val.reshape(val.shape[0], -1)})
+
+    if run is not None:
+        run.log(data, commit=commit)
+
+
+def log_item_old(
         tag: str,
         val: ArrayLike,
         step: Optional[int] = None,
@@ -39,40 +63,84 @@ def log_item(
         tf.summary.scalar(f'{tag}/avg', tf.reduce_mean(val), step=step)
 
 
-def log_dict(d: dict, step: int, prefix: Optional[str] = None):
+def log_dict(
+        d: dict,
+        step: int,
+        prefix: Optional[str] = None,
+        run: Optional[Any] = None,
+        commit: Optional[bool] = True,
+):
     """Create tensorboard summaries for all items in `d`"""
-    for key, val in d.items():
+    for idx, (key, val) in enumerate(d.items()):
+        c = (commit and (idx == len(list(d.keys())) - 1))
         pre = key if prefix is None else f'{prefix}/{key}'
         if isinstance(val, dict):
-            log_dict(val, step=step, prefix=pre)
+            log_dict(val, step=step, prefix=pre, commit=(c and commit))
         else:
-            log_item(pre, val, step=step)
+            log_item(pre, val, step=step, run=run, commit=commit)
 
 
-def log_list(x, step, prefix=None):
-    for t in x:
+def log_list(
+        x,
+        step,
+        prefix=None,
+        run: Optional[Any] = None,
+        commit: Optional[bool] = True,
+):
+    for idx, t in enumerate(x):
         name = getattr(t, 'name', None)
+        c = (commit and (idx == len(x) - 1))
         tag = f'{prefix}/{name}' if name is not None else prefix
-        log_item(tag, t, step=step)
+        log_item(tag, t, step=step, run=run, commit=c)
 
 
 def update_summaries(
         step: int,
+        run: Optional[Any] = None,
         metrics: Optional[dict] = None,
         model: Optional[Model] = None,
         optimizer: Optional[Optimizer] = None,
         prefix: Optional[str] = None,
 ):
     """"Create summary objects."""
+    commit = (model is None and optimizer is None)
+    if run is not None and metrics is not None:
+        if prefix is not None:
+            run.log({prefix: metrics}, commit=commit)
+        else:
+            run.log(metrics, commit=commit)
+
     if metrics is not None:
-        log_dict(metrics, step, prefix=prefix)
+        log_dict(metrics, step, prefix=prefix, run=run)
+        if run is not None and run is wandb.run:
+            if prefix is not None:
+                run.log({prefix: metrics}, commit=commit)
+            else:
+                run.log(metrics, commit=commit)
 
     if model is not None:
-        log_list(model.variables, step=step, prefix='model')
+        commit = (optimizer is not None)
+        log_list(
+            model.variables,
+            run=run,
+            step=step,
+            prefix='model',
+            commit=commit,
+        )
 
     if optimizer is not None:
         ostr = 'optimizer'
         opre = ostr if prefix is None else '/'.join([ostr, prefix])
         lpre = 'lr' if prefix is None else '/'.join(['lr', prefix])
-        tf.summary.scalar(lpre, K.get_value(optimizer.lr), step=step)
-        log_list(optimizer.variables(), step=step, prefix=opre)
+        if run is not None:
+            run.log(
+                {lpre: K.get_value(optimizer.lr), f'{lpre}/iter': step},
+                commit=False
+            )
+        log_list(
+            optimizer.variables(),
+            run=run,
+            step=step,
+            prefix=opre,
+            commit=True
+        )
