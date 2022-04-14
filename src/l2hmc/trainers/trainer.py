50469c21c66def28e8e3dacc0b666840a42c90e9
Sam Foreman <saforem2@gmail.com>
2022-04-14 08:10:20 -0500

    Adds progress trackers to `Trainer` objs
    Former-commit-id: 50742276b84f47f8b5cca8a32a8cdaada6c886ec

--- src/l2hmc/trainers/trainer.py
+++ src/l2hmc/trainers/trainer.py
@@ -4,13 +4,13 @@ trainer.py
 Contains BaseTrainer (ABC) object for training L2HMC dynamics
 """
 from __future__ import absolute_import, division, print_function, annotations
-import time
-from typing import Callable
-import numpy as np
-from src.l2hmc.configs import Steps
-from src.l2hmc.dynamics.pytorch.dynamics import Dynamics, to_u1, random_angle
-from src.l2hmc.loss.pytorch.loss import LatticeLoss
-from src.l2hmc.utils.history import History, StateHistory
+# import time
+# from typing import Callable
+# import numpy as np
+# from src.l2hmc.configs import Steps
+# from src.l2hmc.dynamics.pytorch.dynamics import Dynamics, to_u1, random_angle
+# from src.l2hmc.loss.pytorch.loss import LatticeLoss
+# from src.l2hmc.utils.history import StateHistory
 
 from abc import ABC
 
