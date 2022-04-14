ecbd74e310a1ca4b09116f8f028567c66baceb7b
Sam Foreman <saforem2@gmail.com>
2022-04-14 12:21:36 -0500

    Updates `scripts/tensorflow/main.py`
    Former-commit-id: c71234f529dbaccce99cc0c27a83054bdaddaca5

--- src/l2hmc/scripts/tensorflow/main.py
+++ src/l2hmc/scripts/tensorflow/main.py
@@ -15,6 +15,7 @@ from l2hmc.common import save_and_analyze_data
 from l2hmc.configs import get_jobdir
 from l2hmc.experiment import Experiment
 from l2hmc.trainers.tensorflow.trainer import Trainer
+from l2hmc.utils.tensorflow.utils import get_summary_writer
 
 log = logging.getLogger(__name__)
 
@@ -35,7 +36,7 @@ def evaluate(
         trainer: Trainer,
         job_type: str,
         run: Optional[Any] = None,
-        writer: Optional[Any] = None,
+        # writer: Optional[Any] = None,
         nchains: Optional[int] = 10,
         eps: Optional[TensorLike] = None,
 ) -> dict:
@@ -43,11 +44,13 @@ def evaluate(
     assert job_type in ['eval', 'hmc']
     therm_frac = cfg.get('therm_frac', 0.2)
     jobdir = get_jobdir(cfg, job_type=job_type)
-    # if trainer.rank == 0:
-    #     writer = get_summary_writer(cfg, job_type=job_type)
 
-    if writer is not None:
+    if trainer.rank == 0:
+        writer = get_summary_writer(cfg, job_type=job_type)
+        assert writer is not None
         writer.set_as_default()
+    else:
+        writer = None
 
     output = trainer.eval(run=run,
                           writer=writer,
@@ -81,18 +84,20 @@ def train(
         cfg: DictConfig,
         trainer: Trainer,
         run: Optional[Any] = None,
-        writer: Optional[Any] = None,
+        # writer: Optional[Any] = None,
         nchains: Optional[int] = None,
         **kwargs,
 ) -> dict:
-    writer = None
     nchains = 16 if nchains is None else nchains
     jobdir = get_jobdir(cfg, job_type='train')
-    # if trainer.rank == 0:
-    #     writer = get_summary_writer(cfg, job_type='train')
 
-    if writer is not None:
+    # if writer is not None:
+    if trainer.rank == 0:
+        writer = get_summary_writer(cfg, job_type='train')
+        assert writer is not None
         writer.set_as_default()
+    else:
+        writer = None
 
     output = trainer.train(run=run,
                            writer=writer,
@@ -129,30 +134,33 @@ def main(cfg: DictConfig) -> dict:
     # 3. Run generic HMC as baseline w/ same trajectory length
     # -------------------------------------------------------------
     should_train = (cfg.steps.nera > 0 and cfg.steps.nepoch > 0)
-    if should_train:
-        tw = experiment.get_summary_writer('train')
-        outputs['train'] = train(cfg, trainer, run=run, writer=tw)  # [1.]
+    if should_train:                                                # [1.]
+        # tw = (
+        #     experiment.get_summary_writer('train') if trainer.rank == 0
+        #     else None
+        # )
+        outputs['train'] = train(cfg=cfg, trainer=trainer, run=run)
 
     if trainer.rank == 0:
         nchains = max((4, cfg.dynamics.nchains // 8))
         if should_train and cfg.steps.test > 0:                     # [2.]
             log.warning('Evaluating trained model')
-            ew = experiment.get_summary_writer('eval')
+            # ew = experiment.get_summary_writer('eval')
             outputs['eval'] = evaluate(cfg,
                                        run=run,
                                        eps=None,
-                                       writer=ew,
+                                       # writer=ew,
                                        job_type='eval',
                                        nchains=nchains,
                                        trainer=trainer)
         if cfg.steps.test > 0:                                      # [3.]
             log.warning('Running generic HMC')
             eps = tf.constant(float(cfg.get('eps_hmc', 0.118)))
-            hw = experiment.get_summary_writer('hmc')
+            # hw = experiment.get_summary_writer('hmc')
             outputs['hmc'] = evaluate(cfg=cfg,
                                       run=run,
                                       eps=eps,
-                                      writer=hw,
+                                      # writer=hw,
                                       job_type='hmc',
                                       nchains=nchains,
                                       trainer=trainer)
