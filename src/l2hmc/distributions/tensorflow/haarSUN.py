93c1f8e5aa7a21f49780f1e95cdde96b20155e74
Sam Foreman <saforem2@gmail.com>
2022-06-05 22:33:16 -0500

    Adds `distributions/tensorflow/haarSUN.py`
    Former-commit-id: 76f96c299c3794fcb5cc3b7459181ef171d49cc0

---
+++ src/l2hmc/distributions/tensorflow/haarSUN.py
@@ -0,0 +1,11 @@
+"""
+haarSUN.py
+
+Modifed from pytorch implementation at:
+    https://github.com/CUAI/Equivariant-Manifold-Flows
+"""
+from __future__ import absolute_import, print_function, division, annotations
+from typing import Optional
+
+import tensorflow as tf
+
