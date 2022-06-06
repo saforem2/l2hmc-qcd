00bb553bc271720cc125c904e7509ed3a414a162
Sam Foreman <saforem2@gmail.com>
2022-06-05 22:28:32 -0500

    Updates `distributions/pytorch/haarSUN.py`
    Former-commit-id: b308ca5f126062499ef6d2c70c5ebc4f623955f7

--- src/l2hmc/distributions/pytorch/haarSUN.py
+++ src/l2hmc/distributions/pytorch/haarSUN.py
@@ -5,37 +5,50 @@ Modified from original implementation at:
     https://github.com/CUAI/Equivariant-Manifold-Flows
 """
 from __future__ import absolute_import, print_function, division, annotations
+from typing import Optional
 
 import torch
-import torch.distributions
+import torch.distributions as distributions
 import scipy
 import scipy.linalg
 import numpy as np
 
-from l2hmc.lattice.pytorch.logm import su3_to_eigs_cdesa
+from l2hmc.group.pytorch.logm import su3_to_eigs
 
 Tensor = torch.Tensor
 
 
 class HaarSUN(torch.distributions.Distribution):
-    support = torch.distributions.constraints.real
+    support = torch.distributions.constraints.real  # type:ignore
     has_rsample = True
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, dim: int = 3):
+        self.dim = dim
+        super().__init__()
 
-    def rsample(self, n: int, dim: int):
-        """Produces n uniform samples over SU(dim)."""
-        final = torch.zeros(n, dim, dim).to(torch.complex64)
-        for k in range(n):
-            z = scipy.randn(dim, dim) + 1j * scipy.randn(dim, dim)
-            q, r = scipy.linalg.qr(z / np.sqrt(2.0))
-            d = np.diag(r)
-            ph = d / np.abs(d)
-            q = np.multiply(q, ph, q)
-            q = q / scipy.linalg.det(q) ** (1 / dim)
-            final[k] = torch.tensor(q)
-
+    def rsample(self, nsamples: int) -> Tensor:
+        """Produces n uniform samples over SU(self.dim)."""
+        final = torch.zeros(nsamples, self.dim, self.dim).to(torch.complex64)
+        for k in range(nsamples):
+            z = torch.complex(
+                torch.randn(self.dim, self.dim),
+                torch.randn(self.dim, self.dim),
+            )
+            # z = torch.randn(dim, dim) + torch.randn(dim, dim) *
+            # z = scipy.randn(dim, dim) + 1j * scipy.randn(dim, dim)
+            # d = np.diag(r)
+            # ph = d / np.abs(d)
+            # q = np.multiply(q, ph, q)
+            # q = q / (q.det)
+            # q = q / scipy.linalg.det(q) ** (1 / dim)
+            # final[k] = torch.tensor(q)
+            # q, r = scipy.linalg.qr(z / np.sqrt(2.0))
+            q, r = torch.linalg.qr(z / np.sqrt(2.0))
+            diag = r.diag()
+            ph = diag / diag.abs()
+            q = q * ph * q
+            q = q / (q.det() ** (1 / self.dim))
+            final[k] = q
         return final
 
     def log_prob(self, z: Tensor) -> Tensor:
@@ -43,16 +56,17 @@ class HaarSUN(torch.distributions.Distribution):
         _, n, _ = z.shape
 
         assert n == 3, 'Operation supported only for SU(3)'
-        v = su3_to_eigs_cdesa(z)
+        v = su3_to_eigs(z)
 
         # recall that eigdecomp returns [real1, real2, ..., imag1, imag2, ...]
         # use haar formula from boyda, Π_{i < j} | λ_i - λ_j | ²
-        log_prob = 0.
+        log_prob = torch.tensor(0.)
         for j in range(n):
             for i in range(j):
                 log_prob += torch.log(torch.abs(v[:, i] - v[:, j]) ** 2)
+
         return log_prob
 
-    def rsample_log_prob(self, shape=torch.Size()):
-        z = self.rsample(shape)
+    def rsample_log_prob(self, nsamples: int):
+        z = self.rsample(nsamples)
         return z, self.log_prob(z)
