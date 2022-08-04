236b1fb2bf9b8e11f78e8fc7a7363b7efd10dff8
Sam Foreman <saforem2@gmail.com>
2022-08-04 17:12:41 +0000

    Adds `launch_agents.sh` for launching WandB agents
    Former-commit-id: 0c4bcc58f80763dc9562d19f42612b432a67b7fa

---
+++ src/l2hmc/launch_agents.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
+
+if [[ $(hostname) == theta* ]]; then
+  HOSTFILE=${COBALT_NODEFILE}
+
+elif [[ $(hostname) == x* ]]; then
+  HOSTFILE=${PBS_NODEFILE}
+else
+  echo "HOSTNAME: $(hostname)"
+fi
+
+RANKS=$(cat $HOSTFILE)
+NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
+
+SWEEP_ID=$@
+export SWEEPDIR="logs/sweeps/sweep-${SWEEP_ID}"
+mkdir -p ${SWEEPDIR}
+
+for rank in $RANKS; do
+  TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
+  for ((i=1;i<=$NGPU_PER_RANK;i++)); do
+    LOGFILE="${SWEEPDIR}/${HOST}:gpu-${i}-${TSTAMP}.log"
+    echo "Launching agent on ${HOST}:GPU-${i}"
+    echo "Writing logs to: ${LOGFILE}"
+    CUDA_VISIBLE_DEVICES=$i wandb agent "l2hmc-qcd/l2hmc-qcd/${SWEEP_ID}" > ${LOGFILE} 2>&1 &
+  done
+done
