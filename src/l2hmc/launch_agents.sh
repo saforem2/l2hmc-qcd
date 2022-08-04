305143e8add0cd82173ce932b1451070dc36c0f9
Sam Foreman <saforem2@gmail.com>
2022-08-04 18:43:35 +0000

    Updates `launch_agents.sh`
    Former-commit-id: 085a04b2be2f134bf7ab1989ad20b4ca51d416a6

--- src/l2hmc/launch_agents.sh
+++ src/l2hmc/launch_agents.sh
@@ -3,30 +3,47 @@
 TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
 
 if [[ $(hostname) == theta* ]]; then
+  NRANKS=$(wc -l < ${COBALT_NODEFILE})
   HOSTFILE=${COBALT_NODEFILE}
-
+  NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
+  NGPUS=$((${NRANKS}*${NGPU_PER_RANK}))
+  MPI_COMMAND="mpirun -np $NGPUS -npernode $NGPU_PER_RANK ./set_affinity_gpu_polaris.sh"
+  echo "-----------------------"
+  echo "| Running on ThetaGPU |"
+  echo "-----------------------"
+  echo "HOSTNAME: $(hostname)"
+  # Load conda module and activate base environment
+  eval "$(/lus/theta-fs0/software/thetagpu/conda/2022-07-01/mconda3/bin/conda shell.zsh hook)"
+  module load conda/2022-07-01
+  conda activate base
 elif [[ $(hostname) == x* ]]; then
+  NRANKS=$(wc -l < ${PBS_NODEFILE})
   HOSTFILE=${PBS_NODEFILE}
+  NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
+  NGPUS=$((${NRANKS}*${NGPU_PER_RANK}))
+  MPI_COMMAND="mpiexec -n $NGPUS --ppn $NGPU_PER_RANK -d 16 --envall ./set_affinity_gpu_polaris.sh"
+  # MPI_COMMAND=$(which mpiexec)
+  # MPI_FLAGS="-n ${NGPUS} --ppn ${NGPU_PER_RANK} --envall --hostfile ${HOSTFILE}"
+  echo "-----------------------"
+  echo "| Running on Polaris |"
+  echo "-----------------------"
+  echo "HOSTNAME: $(hostname)"
+  module load conda/2022-07-19
+  conda activate base
 else
   echo "HOSTNAME: $(hostname)"
 fi
 
-RANKS=$(cat $HOSTFILE)
-NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
+# RANKS=$(cat $HOSTFILE)
+# NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
+
+# EXEC="$L2HMC_DIR/launch_on_rank.sh"
 
 SWEEP_ID=$@
-export SWEEPDIR="logs/sweeps/sweep-${SWEEP_ID}"
-mkdir -p ${SWEEPDIR}
+echo SWEEP ID: ${SWEEP_ID}
 
-for rank in $RANKS; do
-  OLD_HOST=$(hostname)
-  ssh $rank
-  echo "$(hostname) (from ${OLD_HOST})"
-  for ((i=1;i<=$NGPU_PER_RANK;i++)); do
-    TSTAMP=$(date "+%Y-%m-%d-%H%M%S-%s")
-    LOGFILE="${SWEEPDIR}/${HOST}:gpu-${i}-${TSTAMP}.log"
-    echo "Launching agent on ${HOST}:GPU-${i}"
-    echo "Writing logs to: ${LOGFILE}"
-    CUDA_VISIBLE_DEVICES=$i wandb agent "l2hmc-qcd/l2hmc-qcd/${SWEEP_ID}" > ${LOGFILE} 2>&1 &
-  done
-done
+if (( ${NGPUS} > 1 )); then
+  ${MPI_COMMAND} wandb agent "l2hmc-qcd/l2hmc-qcd/${SWEEP_ID}"
+else
+  python3 wandb agent ${SWEEPID}
+fi
