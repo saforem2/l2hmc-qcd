be880a85d8baa88007e0560c376a3a0b97c1b820
Sam Foreman <saforem2@gmail.com>
2022-08-10 16:31:31 -0500

    Reduce `filelock` logging
    Former-commit-id: fd8aecb0f5eac7e0c04f2a7edf99eea59984b335

--- src/l2hmc/main.py
+++ src/l2hmc/main.py
@@ -20,6 +20,7 @@ from l2hmc.utils.rich import print_config
 
 log = logging.getLogger(__name__)
 
+logging.getLogger('filelock').setLevel(logging.CRITICAL)
 logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
 logging.getLogger('PIL.PngImagePlugin').setLevel(logging.CRITICAL)
 logging.getLogger('graphviz._tools').setLevel(logging.CRITICAL)
