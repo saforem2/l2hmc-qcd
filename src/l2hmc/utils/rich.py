640c1de2aa983857d1c4ed532b1e8faf4421dad0
Sam Foreman <saforem2@gmail.com>
2023-05-31 08:53:05 -0500

    Update `utils/rich.py`

--- src/l2hmc/utils/rich.py
+++ src/l2hmc/utils/rich.py
@@ -4,19 +4,30 @@ rich.py
 Contains utils for textual layouts using Rich
 """
 from __future__ import absolute_import, annotations, division, print_function
+from contextlib import contextmanager
 from dataclasses import dataclass, field
 import json
 import os
 from pathlib import Path
 import shutil
+import time
 from typing import Optional
 from typing import Any
+from typing import Generator
 
+from enrich import STYLES
+from enrich.console import Console
+from enrich.logging import RichHandler
 from omegaconf import DictConfig, OmegaConf
 import pandas as pd
+import pandas as pd
 import rich
-from rich.console import Console
+from rich import print
+from rich.box import MINIMAL, SIMPLE, SIMPLE_HEAD, SQUARE
+from rich.columns import Columns
 from rich.layout import Layout
+from rich.live import Live
+from rich.measure import Measurement
 from rich.panel import Panel
 from rich.progress import (
     BarColumn,
@@ -26,10 +37,12 @@ from rich.progress import (
     TimeElapsedColumn,
     TimeRemainingColumn,
 )
-from rich.style import Style
 import rich.syntax
 from rich.table import Table
+from rich.table import Table
 import rich.tree
+
+from l2hmc import get_logger
 
 # from l2hmc.configs import Steps
 
@@ -43,50 +56,57 @@ HEIGHT = size.lines
 os.environ['COLUMNS'] = f'{WIDTH}'
 # os.environ['COLUMNS'] = f'{size.columns}'
 
-STYLES = {
-    'color': Style(color='#676767'),
-    'filepath': Style(color='#50FA7B', bold=True),
-    'filename': Style(color='#BD93F9', bold=True),
-    # 'info': Style(color='#29B6F6'),
-    # 'warning': Style(color='#FD971F'),
-    # 'error': Style(color='#FF5252', bold=True),
-    # 'logging.level.warning': Style(color='#FD971F'),
-    'logging.level.info': Style(color='#29B6F6'),
-    'logging.level.warning': Style(color='#FD971F'),
-    'logging.level.error': Style(color='#FF5252'),
-    'yellow': Style(color='#FFFF00'),
-    "time": Style(color="#676767"),
-    'log.time': Style(color='#676767'),
-    # 'repr.attrib_name': Style(color="#676767"),
-    "hidden": Style(color="#383b3d", dim=True),
-    "num": Style(color='#409CDC', bold=True),
-    'repr.number': Style(color='#409CDC', bold=False),
-    "highlight": Style(color="#111111", bgcolor="#FFFF00", bold=True),
-}
+# STYLES = {
+#     # 'color': Style(color='#676767'),
+#     # 'filepath': Style(color='#50FA7B', bold=True),
+#     # 'filename': Style(color='#BD93F9', bold=True),
+#     # # 'info': Style(color='#29B6F6'),
+#     # # 'warning': Style(color='#FD971F'),
+#     # # 'error': Style(color='#FF5252', bold=True),
+#     # # 'logging.level.warning': Style(color='#FD971F'),
+#     # 'logging.level.info': Style(color='#29B6F6'),
+#     # 'logging.level.warning': Style(color='#FD971F'),
+#     # 'logging.level.error': Style(color='#FF5252'),
+#     # 'yellow': Style(color='#FFFF00'),
+#     # "time": Style(color="#676767"),
+#     # 'log.time': Style(color='#676767'),
+#     # # 'repr.attrib_name': Style(color="#676767"),
+#     # "hidden": Style(color="#383b3d", dim=True),
+#     # "num": Style(color='#409CDC', bold=True),
+#     # # 'repr.number': Style(color='#409CDC', bold=False),
+#     # "highlight": Style(color="#111111", bgcolor="#FFFF00", bold=True),
+# }
+#
 
 
 def get_console(**kwargs) -> Console:
     interactive = is_interactive()
     from rich.theme import Theme
     theme = Theme(STYLES)
-    width = int(
-        max(
-            [
-                os.get_terminal_size()[0],
-                shutil.get_terminal_size()[0],
-            ]
-        )
-    )
-    if width <= 100:
-        width = 255
+    # outdir = Path(os.getcwd()).resolve().absolute()
+    # outfile = outdir.joinpath('console.log').as_posix()
+    # width = int(
+    #     max(
+    #         [
+    #             os.get_terminal_size()[0],
+    #             shutil.get_terminal_size()[0],
+    #         ]
+    #     )
+    # )
+    # if width <= 100:
+    #     width = 255
     console = Console(
         force_jupyter=interactive,
         log_path=False,
         theme=theme,
-        width=int(width),
+        soft_wrap=True,
+        # file=outfile,
+        # redirect=(get_world_size() > 1),
+        # width=int(width),
         **kwargs
     )
-
+    # from rich.console import Console
+    # console = Console()
     return console
 
 
@@ -528,3 +548,253 @@ def printarr(*arrs, float_width=6):
 
     finally:
         del frame
+
+        import time
+
+
+# console = Console()
+
+BEAT_TIME = 0.008
+
+COLORS = ["cyan", "magenta", "red", "green", "blue", "purple"]
+
+log = get_logger(__name__)
+handlers = log.handlers
+if (
+        len(handlers) > 0
+        and isinstance(handlers[0], RichHandler)
+):
+    console = handlers[0].console
+else:
+    console = get_console(markup=True)
+
+
+@contextmanager
+def beat(length: int = 1) -> Generator:
+    with console:
+        yield
+    time.sleep(length * BEAT_TIME)
+
+
+class DataFramePrettify:
+    """Create animated and pretty Pandas DataFrame.
+
+    Modified from: https://github.com/khuyentran1401/rich-dataframe
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data you want to prettify
+    row_limit : int, optional
+        Number of rows to show, by default 20
+    col_limit : int, optional
+        Number of columns to show, by default 10
+    first_rows : bool, optional
+        Whether to show first n rows or last n rows, by default True.
+        If this is set to False, show last n rows.
+    first_cols : bool, optional
+        Whether to show first n columns or last n columns, by default True.
+        If this is set to False, show last n rows.
+    delay_time : int, optional
+        How fast is the animation, by default 5.
+        Increase this to have slower animation.
+    clear_console: bool, optional
+         Clear the console before printing the table, by default True.
+         If this is set to False the previous console
+         input/output is maintained
+    """
+
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        row_limit: int = 20,
+        col_limit: int = 10,
+        first_rows: bool = True,
+        first_cols: bool = True,
+        delay_time: int = 5,
+        clear_console: bool = True,
+    ) -> None:
+        self.df = df.reset_index().rename(columns={"index": ""})
+        self.table = Table(show_footer=False)
+        self.table_centered = Columns(
+            (self.table,), align="center", expand=True
+        )
+        self.num_colors = len(COLORS)
+        self.delay_time = delay_time
+        self.row_limit = row_limit
+        self.first_rows = first_rows
+        self.col_limit = col_limit
+        self.first_cols = first_cols
+        self.clear_console = clear_console
+        if first_cols:
+            self.columns = self.df.columns[:col_limit]
+        else:
+            self.columns = list(self.df.columns[-col_limit:])
+            self.columns.insert(0, "index")
+        if first_rows:
+            self.rows = self.df.values[:row_limit]
+        else:
+            self.rows = self.df.values[-row_limit:]
+        if self.clear_console:
+            console.clear()
+
+    def _add_columns(self):
+        for col in self.columns:
+            with beat(self.delay_time):
+                self.table.add_column(str(col))
+
+    def _add_rows(self):
+        for row in self.rows:
+            with beat(self.delay_time):
+
+                if self.first_cols:
+                    row = row[: self.col_limit]
+                else:
+                    row = row[-self.col_limit:]
+
+                row = [str(item) for item in row]
+                self.table.add_row(*list(row))
+
+    def _move_text_to_right(self):
+        for i in range(len(self.table.columns)):
+            with beat(self.delay_time):
+                self.table.columns[i].justify = "right"
+
+    def _add_random_color(self):
+        for i in range(len(self.table.columns)):
+            with beat(self.delay_time):
+                self.table.columns[i].header_style = COLORS[
+                    i % self.num_colors
+                ]
+
+    def _add_style(self):
+        for i in range(len(self.table.columns)):
+            with beat(self.delay_time):
+                self.table.columns[i].style = (
+                    "bold " + COLORS[i % self.num_colors]
+                )
+
+    def _adjust_box(self):
+        for box in [SIMPLE_HEAD, SIMPLE, MINIMAL, SQUARE]:
+            with beat(self.delay_time):
+                self.table.box = box
+
+    def _dim_row(self):
+        with beat(self.delay_time):
+            self.table.row_styles = ["none", "dim"]
+
+    def _adjust_border_color(self):
+        with beat(self.delay_time):
+            self.table.border_style = "bright_yellow"
+
+    def _change_width(self):
+        original_width = Measurement.get(
+            console=console,
+            options=console.options,
+            renderable=self.table
+        ).maximum
+        width_ranges = [
+            [original_width, console.width, 2],
+            [console.width, original_width, -2],
+            [original_width, 90, -2],
+            [90, original_width + 1, 2],
+        ]
+
+        for width_range in width_ranges:
+            for width in range(*width_range):
+                with beat(self.delay_time):
+                    self.table.width = width
+
+            with beat(self.delay_time):
+                self.table.width = None
+
+    def _add_caption(self):
+        if self.first_rows:
+            row_text = "first"
+        else:
+            row_text = "last"
+        if self.first_cols:
+            col_text = "first"
+        else:
+            col_text = "last"
+
+        with beat(self.delay_time):
+            self.table.caption = (
+                f"Only the {row_text} "
+                f"{self.row_limit} rows "
+                f"and the {col_text} "
+                f"{self.col_limit} columns "
+                "is shown here."
+            )
+        with beat(self.delay_time):
+            self.table.caption = (
+                f"Only the [bold green] {row_text} "
+                "{self.row_limit} rows[/bold green] and the "
+                "[bold red]{self.col_limit} {col_text} "
+                "columns[/bold red] is shown here."
+            )
+        with beat(self.delay_time):
+            self.table.caption = (
+                f"Only the [bold magenta not dim] "
+                f"{row_text} {self.row_limit} rows "
+                f"[/bold magenta not dim] and the "
+                f"[bold green not dim]{col_text} "
+                f"{self.col_limit} columns "
+                f"[/bold green not dim] "
+                f"are shown here."
+            )
+
+    def prettify(self):
+        with Live(
+            self.table_centered,
+            console=console,
+            refresh_per_second=self.delay_time,
+            vertical_overflow="ellipsis",
+        ):
+            self._add_columns()
+            self._add_rows()
+            self._move_text_to_right()
+            self._add_random_color()
+            self._add_style()
+            self._adjust_border_color()
+            self._add_caption()
+
+        return self.table
+
+
+def prettify(
+    df: pd.DataFrame,
+    row_limit: int = 20,
+    col_limit: int = 10,
+    first_rows: bool = True,
+    first_cols: bool = True,
+    delay_time: int = 5,
+    clear_console: bool = True,
+):
+    """Create animated and pretty Pandas DataFrame
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data you want to prettify
+    row_limit : int, optional
+        Number of rows to show, by default 20
+    col_limit : int, optional
+        Number of columns to show, by default 10
+    first_rows : bool, optional
+        Whether to show first n rows or last n rows, by default True. If this is set to False, show last n rows.
+    first_cols : bool, optional
+        Whether to show first n columns or last n columns, by default True. If this is set to False, show last n rows.
+    delay_time : int, optional
+        How fast is the animation, by default 5. Increase this to have slower animation.
+    clear_console: bool, optional
+        Clear the console before printing the table, by default True. If this is set to false the previous console input/output is maintained
+    """
+    if isinstance(df, pd.DataFrame):
+        DataFramePrettify(
+            df, row_limit, col_limit, first_rows, first_cols, delay_time,clear_console
+        ).prettify()
+
+    else:
+        # In case users accidentally pass a non-datafame input, use rich's print instead
+        print(df)
