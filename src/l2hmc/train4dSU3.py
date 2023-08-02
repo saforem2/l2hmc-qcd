6f0afd25d18c80134a64dd8d56fc00f579d4d3df
Sam Foreman <saforem2@gmail.com>
2023-08-02 08:55:00 -0500

    Update `train4dSU3.py`

--- src/l2hmc/train4dSU3.py
+++ src/l2hmc/train4dSU3.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import yaml
-from l2hmc import get_logger
 
+import l2hmc.group.su3.pytorch.group as g
+from l2hmc import get_logger
 from l2hmc.common import grab_tensor, print_dict
 from l2hmc.configs import dict_to_list_of_overrides, get_experiment
-from l2hmc.experiment.pytorch.experiment import evaluate  # noqa  # noqa
+from l2hmc.experiment.pytorch.experiment import Experiment, evaluate  # noqa  # noqa
 from l2hmc.utils.dist import setup_torch
 from l2hmc.utils.plot_helpers import set_plot_style
 
@@ -20,7 +21,8 @@ lt.monkey_patch()
 os.environ['CUDA_VISIBLE_DEVICES'] = '6'
 os.environ['COLORTERM'] = 'truecolor;'
 os.environ['MASTER_PORT'] = '5433'
-os.environ['MPLBACKEND'] = 'module://matplotlib-backend-kitty'
+# os.environ['MPLBACKEND'] = 'module://matplotlib-backend-kitty'
+# plt.switch_backend('module://matplotlib-backend-kitty')
 
 log = get_logger(__name__)
 
@@ -36,11 +38,31 @@ from l2hmc.utils.plot_helpers import (  # noqa
 )
 
 
+def savefig(fig: plt.Figure, fname: str, outdir: os.PathLike):
+    pngfile = Path(outdir).joinpath(f"pngs/{fname}.png")
+    svgfile = Path(outdir).joinpath(f"svgs/{fname}.svg")
+    pngfile.parent.mkdir(exist_ok=True, parents=True)
+    svgfile.parent.mkdir(exist_ok=True, parents=True)
+    fig.savefig(svgfile, transparent=True, bbox_inches='tight')
+    fig.savefig(pngfile, transparent=True, bbox_inches='tight', dpi=300)
+
+
 def plot_metrics(metrics: dict, title: Optional[str] = None, **kwargs):
+    from l2hmc.utils.rich import is_interactive
+    outdir = Path(f"./plots-4dSU3/{title}")
+    outdir.mkdir(exist_ok=True, parents=True)
     for key, val in metrics.items():
         fig, ax = plot_metric(val, name=key, **kwargs)
         if title is not None:
             ax.set_title(title)
+        log.info(f"Saving {key} to {outdir}")
+        savefig(fig, f"{key}", outdir=outdir)
+        # fpath = outdir.joinpath(f"{key}")
+        # plt.savefig(f"{fpath}.svg", bbox_inches='tight')
+        # plt.savefig(f"{fpath}.png", bbox_inches='tight', dpi=300)
+        # log.info(f"Saving {title} {key} plot to {fpath}")
+        if not is_interactive():
+            plt.show()
 
 
 def plot_metric(
@@ -70,14 +92,16 @@ def main():
     set_plot_style()
 
     su3conf = Path('./conf/su3-min.yaml')
-    su3conf
-    su3conf.is_file()
     with su3conf.open('r') as stream:
         conf = dict(yaml.safe_load(stream))
 
+    log.info(conf)
     overrides = dict_to_list_of_overrides(conf)
     ptExpSU3 = get_experiment(overrides=[*overrides], build_networks=True)
     state = ptExpSU3.trainer.dynamics.random_state(6.0)
+    assert isinstance(state.x, torch.Tensor)
+    assert isinstance(state.beta, torch.Tensor)
+    assert isinstance(ptExpSU3, Experiment)
     xhmc, history_hmc = evaluate(
         nsteps=10,
         exp=ptExpSU3,
@@ -90,7 +114,17 @@ def main():
         nprint=2,
         grab=True
     )
+    xhmc = ptExpSU3.trainer.dynamics.unflatten(xhmc)
+    log.info(f"checkSU(x_hmc): {g.checkSU(xhmc)}")
     plot_metrics(history_hmc, title='HMC', marker='.')
+    # ptExpSU3.trainer.dynamics.init_weights(
+    #     method='uniform',
+    #     min=-1e-16,
+    #     max=1e-16,
+    #     bias=True,
+    #     # xeps=0.001,
+    #     # veps=0.001,
+    # )
     xeval, history_eval = evaluate(
         nsteps=10,
         exp=ptExpSU3,
@@ -101,13 +135,35 @@ def main():
         nprint=2,
         grab=True,
     )
+    xeval = ptExpSU3.trainer.dynamics.unflatten(xeval)
+    log.info(f"checkSU(x_eval): {g.checkSU(xeval)}")
     plot_metrics(history_eval, title='Evaluate', marker='.')
+
+    history = {}
     x = state.x
     for step in range(20):
-        log.info(f"train step: {step}")
+        log.info(f'TRAIN STEP: {step}')
         x, metrics = ptExpSU3.trainer.train_step((x, state.beta))
-        if step % 5 == 0:
+        if (step > 0 and step % 2 == 0):
             print_dict(metrics, grab=True)
+        if (step > 0 and step % 1 == 0):
+            for key, val in metrics.items():
+                try:
+                    history[key].append(val)
+                except KeyError:
+                    history[key] = [val]
+
+    x = ptExpSU3.trainer.dynamics.unflatten(x)
+    log.info(f"checkSU(x_train): {g.checkSU(x)}")
+    plot_metrics(history, title='train', marker='.')
+    #
+    # for step in range(20):
+    #     log.info(f"train step: {step}")
+    #     x, metrics = ptExpSU3.trainer.train_step((x, state.beta))
+    #     if step % 5 == 0:
+    #         print_dict(metrics, grab=True)
+
+    return x, history
 
 
 if __name__ == '__main__':
